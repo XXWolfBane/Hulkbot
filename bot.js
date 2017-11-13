871f372a9badffb8a3d31d93aea0a2211cb51f1d
FreakingHulk-Gaming <gideon.lingle14@gmail.com>
2017-11-12 22:42:03 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,19 +10,23 @@ bot.on("ready", () => {
 bot.on("message", message => {
     if (message.content == prefix + "ping") {
       message.reply("pong!")
+     console.log("Someone used the ping command.")
     } 
 });
 
 bot.on("message", message => {
   if (message.content == prefix + "help") {
     message.reply("ping(owner only for now :wink:), help, perms")
+   console.log("Someone used the help command.")
   }
 });
 
 bot.on("message", message => {
   if (message.content == prefix + "perms") {
+   bot.startTyping();
      message.author.sendMessage("No permissions yet. Contact FreakingHulk Gaming#6545 for details.")
-  }
+   console.log(message.author.username)
+   bot.stopTyping();  }
 });
 
 bot.login(process.env.botToken);
