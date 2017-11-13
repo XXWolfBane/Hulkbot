bf053e0d64c145905a13572d799b8c7d590fff5d
FHGDev <gideon.lingle14@gmail.com>
2017-11-13 13:26:23 -0600

    Added kick command

--- bot.js
+++ bot.js
@@ -29,6 +29,12 @@ bot.on("message", message => {
    if (message.content == prefix + "joinserver") {
       message.reply(" Use this to join the help server: https://discord.gg/XvMA2rJ")
    }
+    if (message.content.startsWith(prefix + "kick")) {
+     const member = message.mentions.members.first();
+     member.kick().then((member) => {
+      message.channel.send(":wave: " + member.displayName + " has been successfully kicked :point_right: ");
+        }).catch(() => {
+      message.channel.send("Access Denied");
 })
 
 bot.login(process.env.botToken);
