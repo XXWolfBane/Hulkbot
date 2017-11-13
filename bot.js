36c86e782fb95005653b42ac62eef83ca6c01653
FreakingHulk-Gaming <gideon.lingle14@gmail.com>
2017-11-12 20:41:42 -0600

    More fixes

--- bot.js
+++ bot.js
@@ -3,7 +3,8 @@ const bot = new discord.Client()
 const prefix = "h!"
 
 bot.on("ready", () => {
- console.log("Bot is started. Get ready for some sweet commands!") bot.user.setGame("h!help")
+ console.log("Bot is started. Get ready for some sweet commands!") 
+ bot.user.setGame("h!help")
 });
 
 bot.on("message", message => {
