015066138873e452d52dc7dfd528c4ef41376354
FreakingHulk-Gaming <gideon.lingle14@gmail.com>
2017-11-12 20:39:10 -0600

    Bug fixes

--- bot.js
+++ bot.js
@@ -2,14 +2,15 @@ const discord = require('discord.js')
 const bot = new discord.Client()
 const prefix = "h!"
 
-bot.on(("ready", () => {
+bot.on("ready", () => {
  console.log("Bot is started. Get ready for some sweet commands!") bot.user.setGame("h!help")
 });
 
-bot.on(("message", message => {
+bot.on("message", message => {
     if (message.content == prefix + "ping") {
       message.reply("pong!")
     } 
 });
 
 bot.login(process.env.token)
+
