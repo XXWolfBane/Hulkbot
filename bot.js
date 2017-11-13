31261a9235788e18fa08954ebf4e14131513ccfa
FHGDev <gideon.lingle14@gmail.com>
2017-11-13 13:53:59 -0600

    Will now console.log bot's name, ping, and owner name.

--- bot.js
+++ bot.js
@@ -1,10 +1,14 @@
 const discord = require('discord.js')
 const bot = new discord.Client()
 const prefix = "h!"
+const owner = "FreakingHulk Gaming#6545"
 
 bot.on("ready", () => {
  console.log("Bot is started. Get ready for some sweet commands!") 
  bot.user.setGame("h!help | Kick command is now active")
+ console.log("Bot ping: " + bot.ping + "ms")
+ console.log("Bot name: " + bot.user.username)
+ console.log("Bot owner: " + owner)
 });
 
 bot.on("message", message => {
