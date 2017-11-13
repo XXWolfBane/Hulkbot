1acdeb8ac58df64d17c65f181fd5d677f80f3c22
RHG <huskey.rusty@gmail.com>
2017-11-13 16:02:32 -0500

    bug fix

--- commands/info.js
+++ commands/info.js
@@ -4,7 +4,7 @@ module.exports.run = (bot, message, args) => {
   msgArray.push(`I'm currently running on Discord.js version 11.2.1, which utilizes the latest version of Discord.js, an *unofficial* Discord libary by hydrabolt.`);
   msgArray.push(`The developer of my source code is <@242734840829575169>.`);
   msgArray.push(`To see what I can do, use h!help`);
-  msgArray.push(`My invocation method is using prefixes, currently, I only respond to messages beginning with ${config.prefix}`);
+  msgArray.push(`My invocation method is using prefixes, currently, I only respond to messages beginning with h!`);
   message.channel.send(msgArray).then(m => m.delete(15000))
 }
 
