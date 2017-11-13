8f9c6b12671d0300f42683f903acffa568b6067b
FreakingHulk-Gaming <gideon.lingle14@gmail.com>
2017-11-12 20:00:02 -0600

    Core bot stuff

--- bot.js
+++ bot.js
@@ -2,6 +2,8 @@ const discord = require('discord.js')
 const bot = new discord.Client()
 const prefix = "h!"
 
+bot.on(console.log("Bot is started. Get ready for some sweet commands!") bot.user.setGame("h!help"))
+
 bot.on((message), message => {
     if (message.content == prefix + "ping") {
       message.reply("pong!")
