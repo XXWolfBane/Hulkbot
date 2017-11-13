dae8443d7663bf8b948443cddb8b6eadc97d02ed
RHG <huskey.rusty@gmail.com>
2017-11-13 16:08:50 -0500

    Create ping.js

---
+++ commands/ping.js
@@ -0,0 +1,9 @@
+module.exports.run = (bot, message, args) => {
+  message.channel.send("PONG! :ping_pong: My ping is " + bot.ping + "ms!")
+}
+
+module.exports.help = {
+  name: "ping",
+  usage: ``,
+  information: ""
+}
