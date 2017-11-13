76d6b4c64ebfbca08c5e22fc1e874689f5a26720
RHG <huskey.rusty@gmail.com>
2017-11-13 15:47:57 -0500

    Command Handler

--- bot.js
+++ bot.js
@@ -3,6 +3,17 @@ const bot = new discord.Client()
 const prefix = "h!"
 const owner = "FreakingHulk Gaming#6545"
 
+// Gather commands
+bot.commands = new Discord.Collection();
+
+require('fs').readdir("./commands/", (err, files) => {
+	console.log("Loading commands...")
+  if (err) return console.log(`Command loading failed!`);
+  files.filter(f => f.split(".").pop() === "js").forEach((f, i) => {
+    bot.commands.set(require(`./commands/${f}`).help.name, require(`./commands/${f}`));
+  })
+})
+
 bot.on("ready", () => {
  console.log("Bot is started. Get ready for some sweet commands!") 
  bot.user.setGame("h!help | PIZZA IS BETTER")
@@ -29,6 +40,17 @@ bot.on("message", message => {
     }
    if (message.content == prefix + "joinserver") {
       message.reply("use this to join the help server: https://discord.gg/XvMA2rJ")
+   }
+ 
+  let mArray = message.content.split(" ");
+  let args = mArray.slice(1);
+
+  let cmd = commands.get(mArray[0].slice(prefix.length));
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return
+    
+  if(cmd) {
+    cmd.run(bot, message, args);
    }
 })
 
