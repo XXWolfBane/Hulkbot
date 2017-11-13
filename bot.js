349d17af2bf36656a605ab6bc454577be0b08088
FHGDev <gideon.lingle14@gmail.com>
2017-11-13 13:50:32 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ bot.on("ready", () => {
 
 bot.on("message", message => {
     if (message.content == prefix + "ping") {
-      message.reply("PONG! :ping_pong: My ping is " + bot.ping + "ms!")
+      message.channel.send("PONG! :ping_pong: My ping is " + bot.ping + "ms!")
      console.log(message.author.username + " used the ping command.")
     } 
     if (message.content == prefix + "help") {
