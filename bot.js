8c2186bc1d2739b968e877ab7f84a22a000dc4a6
FHGDev <gideon.lingle14@gmail.com>
2017-11-13 00:15:36 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,6 @@ bot.on("message", message => {
     if (message.content == prefix + "ping") {
       message.reply("pong!")
      console.log(message.author.username + " used the ping command.")
-     bot.channels.get(362713361332568075).sendMessage(message.author.username + " used the ping command!")
     } 
 });
 
@@ -19,7 +18,6 @@ bot.on("message", message => {
   if (message.content == prefix + "help") {
     message.reply("ping(owner only for now :wink:), help, perms")
    console.log(message.author.username + " used the help command.")
-   bot.channels.get(362713361332568075).sendMessage(message.author.username + " used the help command!")
   }
 });
 
@@ -27,7 +25,6 @@ bot.on("message", message => {
   if (message.content == prefix + "perms") {
      message.author.sendMessage("No permissions yet. Contact FreakingHulk Gaming#6545 for details.")
    console.log(message.author.username + " used the perms command.")
-   bot.channels.get(362713361332568075).sendMessage(```message.author.username + " used the permissions command!"```)
     }
 });
 
@@ -35,7 +32,6 @@ bot.on("message", message => {
    if (message.content == prefix + "invite") {
     message.reply("I can't use invite links. :frowning2: Here's an OAuth2 link instead! https://discordapp.com/oauth2/authorize?&client_id=294194506113220608&scope=bot&permissions=0")
     console.log(message.author.username + " used the invite command.") 
-    bot.channels.get(362713361332568075).sendMessage(```message.author.username + " used the invite command!"```)
    }
 });
 
