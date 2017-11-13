273565c4eef9cd6c6af3e0ce201cafedbee7a2e9
RHG <huskey.rusty@gmail.com>
2017-11-13 16:09:42 -0500

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,9 @@
+module.exports.run = (bot, message, args) => {
+  message.reply("Commands are: ```ping, help, perms, kick```")
+}
+
+module.exports.help = {
+  name: "help",
+  usage: ``,
+  information: ""
+}
