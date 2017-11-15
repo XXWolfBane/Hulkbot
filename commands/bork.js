9c90ecac99acad87067076810fcc6abebe5e6f67
FHGDev <gideon.lingle14@gmail.com>
2017-11-15 13:00:57 -0600

    Update bork.js

--- commands/bork.js
+++ commands/bork.js
@@ -2,7 +2,7 @@ module.exports.run = (bot, message, args) => {
   message.channel.send("BOOOOORRRRRKKKKK")
 }
 
-module.exports.bork = {
+module.exports.help = {
   name: "Bork",
   usage: ``,
   information: ""
