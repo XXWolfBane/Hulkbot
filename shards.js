09d1b32c7bd3cade6e83da1fe5374c9f6bace4e9
FHGDev <gideon.lingle14@gmail.com>
2017-11-20 12:34:48 -0600

    Update shards.js

--- shards.js
+++ shards.js
@@ -1,3 +1,4 @@
 const Discord = require('discord.js');
-const Manager = new Discord.ShardingManager('./bot.js');
+const main = require(./package.json.main)
+const Manager = new Discord.ShardingManager();
 Manager.spawn(2); // This example will spawn 2 shards (5,000 guilds);
