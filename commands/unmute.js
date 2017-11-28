4afc2826efa74039a9cbe943a28f2fe0e56abde5
RHG <huskey.rusty@gmail.com>
2017-11-27 21:11:35 -0500

    Update unmute.js

--- commands/unmute.js
+++ commands/unmute.js
@@ -3,8 +3,12 @@ const Discord = require('discord.js');
 
 
 module.exports.run = (bot, message, args) => {
-			member.removeRole(muteRole.id);
-			message.channel.send(`${member.user.username}, is now unmuted`).then(m => m.delete(2500))
+	let muteRole = message.guild.roles.find("name", "Muted");
+
+	if(!member.roles.find("name", "Muted")) return message.channel.send(`:x: ${member.user.username}, isn't muted!`).then(m => m.delete(2500))
+		
+	member.removeRole(muteRole.id);
+	message.channel.send(`${member.user.username}, is now unmuted`).then(m => m.delete(2500))
 }
 
 module.exports.help = {
