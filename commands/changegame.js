7c7d04842a55eb11c0618e219e1f95715a255fc6
FHGDev <gideon.lingle14@gmail.com>
2017-11-29 17:25:04 -0600

    Update changegame.js

--- commands/changegame.js
+++ commands/changegame.js
@@ -1,9 +1,13 @@
+const config = require(../config.json)
 module.exports.run = (bot, message, args) => {
+ if (message.author.id == config.oid) {
  if (!args) return message.channel.send(`:ok_hand: Okay, I will set my playing status back to normal!`)
  var game = args.join(' ')
  bot.user.setGame(game)
  message.channel.send(`:ok_hand: Okay, I will set my playing status to ${game}!`)
-}
+}} else {
+  message.channel.send("Nope!")
+} 
 
 module.exports.help = {
   name: "changegame",
