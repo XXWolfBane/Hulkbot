57d42d2cbd591af40f57d5c485d5f57bdb3cf753
FHGDev <gideon.lingle14@gmail.com>
2017-11-29 15:20:08 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -125,8 +125,8 @@ bot.on("message", (message) => {
     });
   }
   if (message.content.startsWith(prefix + "giverole")) {
-    let member = message.mentions.members.first();
-    let role = message.mentions.roles.first();
+    var member = message.mentions.members.first();
+    var role = message.mentions.roles.first();
     member.addRole(role).then(console.log(member.displayName + " was given " + role).catch(message.reply("I can't assign roles without permissions. Please move the Hulkbot role above all other roles. Thanks! :grin:")))
     message.channel.send("User was given role.")
   }
