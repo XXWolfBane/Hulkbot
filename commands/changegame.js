c40fc85beb6377a94feab3068c25e9254bfb637b
FHGDev <gideon.lingle14@gmail.com>
2017-11-29 17:26:50 -0600

    Update changegame.js

--- commands/changegame.js
+++ commands/changegame.js
@@ -1,4 +1,4 @@
-const config = require(../config.json)
+const config = require(./config.json)
 module.exports.run = (bot, message, args) => {
  if (message.author.id == config.oid) {
  if (!args) return message.channel.send(`:ok_hand: Okay, I will set my playing status back to normal!`)
