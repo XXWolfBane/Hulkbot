f6a37c8476deaf7a7e18067da074a22ba9acbe9e
RHG <huskey.rusty@gmail.com>
2017-11-29 18:55:47 -0500

    Update logger.js

--- logger.js
+++ logger.js
@@ -1,3 +1,29 @@
-SOON
+const config = require("./config.json");
+const discord = require('discord.js');
 
-~RHG
+module.exports = (bot, custom) => {
+  const messages = await bot.channels.get(config.logchannel).fetchMessages({limit: 5});
+  const log = messages.filter(m => m.author.id === bot.user.id &&
+    m.embeds[0] &&
+    m.embeds[0].type === 'rich' &&
+    m.embeds[0].footer &&
+    m.embeds[0].footer.text.startsWith('Case')
+  ).first();
+  var thisCase = 0
+  var foot = ``
+  if (log) {
+    thisCase = /Case\s(\d+)/.exec(log.embeds[0].footer.text);
+    foot = `Case ${parseInt(thisCase[1]) + 1}`
+  } else {
+    foot = `Case 1`
+  }
+  const reason = reaso || `No reason given.`
+  if (!custom) custom = ""
+  let embed = new discord.RichEmbed()
+    .setTimestamp()
+    .setAuthor(`${bot.user.username} Logs ✍`, bot.user.avatarURL)
+    .setColor("7289DA")
+    .setDescription(`${custom}`)
+    .setFooter(foot);
+  bot.channels.get("356181785899565077").send({embed});
+};
