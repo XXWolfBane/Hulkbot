ea28907b8e7f98c45e54f323ebdbebc4d8a5e1c9
FHGDev <gideon.lingle14@gmail.com>
2017-11-30 15:49:58 -0600

    Create clearlogs.js

---
+++ commands/clearlogs.js
@@ -0,0 +1,9 @@
+const config = require('../config.json')
+
+module.exports.run = (bot, message, args) => {
+  if (message.channel.id == config.logid) {
+    if (message.content == prefix + "clearlogs") {
+      message.channel.send("h!purge 90")
+    }
+  }
+}
