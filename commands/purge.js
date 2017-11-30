a3d27826882fc2fb8b7cfbe764cdf0e371fb1ff0
FHGDev <gideon.lingle14@gmail.com>
2017-11-30 15:59:57 -0600

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -3,19 +3,19 @@ module.exports.run = (bot, message, args) => {
     return message.channel.send('Please define a number..')
   }
 
-  var amm = args[0]
-  if (amm < 3) {
-    return message.channel.send('The minimum is ammount you can delete is 3.').then(m => m.delete(2500))
+  var am = args[0]
+  if (am < 3) {
+    return message.channel.send('The minimum is amount you can delete is 3.')
   }
-  if (amm > 190) {
-    message.channel.send("The maximum is ammount you can delete is 90.").then(m => m.delete(2500))
+  if (am > 190) {
+    message.channel.send("The maximum is amount you can delete is 90.")
     return;
   }
-  message.channel.send(":exclamation: Beginning to purge " + amm + " messages... :exclamation:").then(m => m.delete(2500))
+  message.channel.send(":exclamation: Beginning to purge " + am + " messages... :exclamation:").then(m => m.delete(2500))
 
   setTimeout(function() {
-	message.channel.fetchMessages({limit: amm}).then(m => message.channel.bulkDelete(m))
-	message.channel.send("Done! Purged " + amm + " messages.").then(m => m.delete(2500))
+	message.channel.fetchMessages({limit: am}).then(m => message.channel.bulkDelete(m))
+	message.channel.send("Done! Purged " + am + " messages.")
   }, 3000);
 
 
@@ -23,6 +23,6 @@ module.exports.run = (bot, message, args) => {
 
 module.exports.help = {
   name: "purge",
-  usage: `[ammount]`,
-  information: "Remove x ammount of messages"
+  usage: `[amount]`,
+  information: "Remove x amount of messages"
 }
