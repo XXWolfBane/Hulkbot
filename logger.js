4dc5b649066c8dde264ae11c250e261a651331a2
RHG <huskey.rusty@gmail.com>
2017-11-29 19:14:56 -0500

    Update logger.js

--- logger.js
+++ logger.js
@@ -2,30 +2,29 @@ const config = require("./config.json");
 const discord = require('discord.js');
 let logchannel = "356181785899565077";
 
-async function baselogger(bot, desc) => {
-  let messages = await bot.channels.get(logchannel).fetchMessages({limit: 5});
-  let log = messages.filter(m => m.author.id === bot.user.id &&
-    m.embeds[0] &&
-    m.embeds[0].type === 'rich' &&
-    m.embeds[0].footer &&
-    m.embeds[0].footer.text.startsWith('Case')
-  ).first();
+async function baselogger(bot, desc) {
+  //let messages = await bot.channels.get(logchannel).fetchMessages({limit: 5});
+  //let log = messages.filter(m => m.author.id === bot.user.id &&
+  //  m.embeds[0] &&
+  //  m.embeds[0].type === 'rich' &&
+  //  m.embeds[0].footer &&
+  //  m.embeds[0].footer.text.startsWith('Case')
+  //).first();
   var thisCase = 0
   var foot = ``
-  if (log) {
-    thisCase = /Case\s(\d+)/.exec(log.embeds[0].footer.text);
-    foot = `Case ${parseInt(thisCase[1]) + 1}`
-  } else {
-    foot = `Case 1`
-  }
-  const reason = reaso || `No reason given.`
-  if (!custom) custom = ""
+  //if (log) {
+  //  thisCase = /Case\s(\d+)/.exec(log.embeds[0].footer.text);
+  //  foot = `Case ${parseInt(thisCase[1]) + 1}`
+  //} else {
+  //  foot = `Case 1`
+  //}
+  //const reason = reaso || `No reason given.`
   let embed = new discord.RichEmbed()
     .setTimestamp()
-    .setAuthor(`${bot.user.username} Logs ✍`, bot.user.avatarURL)
+    .setAuthor(`Hulk Logs ✍`, bot.user.avatarURL)
     .setColor("7289DA")
     .setDescription(desc)
-    .setFooter(foot);
+  //  .setFooter(foot);
   
   bot.channels.get(logchannel).send({embed});
 };
