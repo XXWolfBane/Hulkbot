21d50001b8052349aed1a1b968e89865d7d0ff30
FHGDev <gideon.lingle14@gmail.com>
2017-11-30 14:58:53 -0600

    Update coinflip.js

--- commands/coinflip.js
+++ commands/coinflip.js
@@ -1,9 +1,9 @@
 module.exports.run = (bot, message, args) => {
   const result = Math.round(Math.random());
   if (result) {
-    message.channel.send("**Coin Flip:**\n\nThe coin landed on heads.").then(m => m.delete(2500))
+    message.channel.send("**Coin Flip:**\n\nThe coin landed on heads.")
   } else {
-    message.channel.send("**Coin Flip:**\n\nThe coin landed on tails.").then(m => m.delete(2500))
+    message.channel.send("**Coin Flip:**\n\nThe coin landed on tails.")
   }
 }
 
