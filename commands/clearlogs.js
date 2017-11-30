59e597972ae40ddb1b9242a470344047ecf6adab
FHGDev <gideon.lingle14@gmail.com>
2017-11-30 16:22:10 -0600

    Create clearlogs.js

---
+++ commands/clearlogs.js
@@ -0,0 +1,11 @@
+module.exports.run => (bot, message, args) {
+  if (message.channel.id == config.logid) {
+    if (message.content == prefix + "")
+  }
+}
+
+module.exports.help = {
+  name: "clearlogs"
+  description: "clear the log channel."
+  usage: "h!clearlogs"
+}
