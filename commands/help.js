9956d943cf4bddab4c4b00bba8eef257d20e5119
FHGDev <gideon.lingle14@gmail.com>
2017-11-30 15:02:21 -0600

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -18,7 +18,7 @@ module.exports.run = (bot, message, args) => {
       },
       {
         name: "Fun",
-        value: "Coming soon"
+        value: "coinflip"
       },
       {
         name: "More",
