8ca82a3260560ec1f39add482bd8f0e5ac9de024
FHGDev <gideon.lingle14@gmail.com>
2017-11-30 13:40:13 -0600

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -23,7 +23,7 @@ module.exports.run = (bot, message, args) => {
       {
         name: "More"
         value: "Additional commands at https://bot.hulkbot.ml"
-        
+       }
       }
     ],
     timestamp: new Date(),
