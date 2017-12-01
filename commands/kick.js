874ce11b0f272f6261f90c03c2097ed18dea1c83
FHGDev <gideon.lingle14@gmail.com>
2017-12-01 10:26:03 -0600

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,7 +1,7 @@
 const config = require('../config.json')
 
 module.exports.run = (bot, message, args) => {
-  if (message.author.hasPermission("KICK_MEMBERS") {
+  if (message.author.hasPermission("KICK_MEMBERS")) {
     if (message.content == prefix + "kick") {
     let member = message.mentions.members.first();
     
@@ -10,5 +10,4 @@ module.exports.run = (bot, message, args) => {
   } else {
     message.channel.send("You don't have permission to kick people. If there's someone who needs to be kicked, contact an administrator or a moderator.")
   }
-}
 }
