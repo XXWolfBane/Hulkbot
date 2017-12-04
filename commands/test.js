f8a629768769c5a442553b2275c18774625d4029
FHGDev <gideon.lingle14@gmail.com>
2017-12-04 14:06:17 -0600

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,10 +1,11 @@
 const oid = process.env.oid
+const prefix = require('../config.json').prefix
 
 module.exports.run = (bot, message, args) => {
   let auth = message.author.id
   if (auth == oid) {
     if (message.content == prefix + "test") {
-      message.channel.send("Test Successfull!")
+      message.channel.send("Test Successful!")
     }
   }
   else {
