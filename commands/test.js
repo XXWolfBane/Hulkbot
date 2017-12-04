3c46a11b1628128f2dba7f55defc1c5ebd761b57
FHGDev <gideon.lingle14@gmail.com>
2017-12-04 14:02:39 -0600

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,5 +1,15 @@
+const oid = process.env.oid
+
 module.exports.run = [bot, message, args] => {
-  
+  let auth = message.author.id
+  if (auth == oid) {
+    if (message.content == prefix + "test") {
+      message.channel.send("Test Successfull!")
+    }
+  }
+  else {
+    message.channel.send("Command is experimental. Owner only.")
+  }
 }
 
 module.exports.help = {
