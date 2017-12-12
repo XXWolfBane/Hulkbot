5c314cec0965cbd1e6e38e0e543e8a7c69a311f1
FHGDev <gideon.lingle14@gmail.com>
2017-12-11 20:37:27 -0600

    Update fml.js

--- commands/fml.js
+++ commands/fml.js
@@ -1,10 +1,12 @@
 module.exports.run = (bot, message, args) => {
   require('request')('http://api.fmylife.com/', function(error, response, body) {
+	  if (message.content == prefix + "fml") {
 	if (!error && response.statusCode == 200) {
 	  message.channel.send(`**FML:**\n\n${JSON.parse(body).joke}`)
     } else {
       message.channel.send(`**FML:**\n\n I couldn't think of a FML Joke...`)
     }
+}
   });
 }
 
