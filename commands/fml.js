df4ecaa6e50368676778f92afb8cc7625f75dcc4
FHGDev <gideon.lingle14@gmail.com>
2017-12-11 20:34:01 -0600

    Update fml.js

--- commands/fml.js
+++ commands/fml.js
@@ -1,1 +1,21 @@
+module.exports.run = (bot, message, args) => {
+  require('request')('http://api.fmylife.com/', function(error, response, body) {
+	if (!error && response.statusCode == 200) {
+	  message.channel.send(`**FML:**\n\n${JSON.parse(body).joke}`)
+    } else {
+      message.channel.send(`**FML:**\n\n I couldn't think of a FML Joke...`)
+    }
+  });
+}
 
+module.exports.help = {
+  name: "yomomma",
+  usage: ``,
+  information: "I'll get a random yo momma joke for you."
+}
+
+module.exports.settings = {
+  permission: "All",
+  deleteresponder: true,
+  category: "Fun"
+}
