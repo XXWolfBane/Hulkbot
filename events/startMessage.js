f3752e6461f697b4a2c9b8346e1444c7545e3ca3
FHGDev <gideon.lingle14@gmail.com>
2017-12-12 14:12:45 -0600

    Update startMessage.js

--- events/startMessage.js
+++ events/startMessage.js
@@ -1,9 +1,10 @@
 const discord = require('discord.js')
 const client = new discord.Client
+const v = require('../config.json').version
 
 client.on('ready', () => {
-var channel = client.channels.get('my-unique-channel-id');
-  channel.sendMessage("Hello world");
+var channel = client.channels.get('387342809390120970');
+  channel.send("Hulkbot v" + v + " loaded!");
 });
 
 client.login(process.env.token)
