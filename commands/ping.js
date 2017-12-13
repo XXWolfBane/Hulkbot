f6eb10edb8185348d378276cad27db997648d41e
FHGDev <gideon.lingle14@gmail.com>
2017-12-13 11:31:34 -0600

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,9 +1,5 @@
-const prefix = "j!"
-
 module.exports.run = (bot, message, args) => {
-  if (message.content == prefix + "ping") {
   message.channel.send("PONG! :ping_pong: My ping is " + bot.ping + "ms!")
- }
 }
 
 module.exports.help = {
