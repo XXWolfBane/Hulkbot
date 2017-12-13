41a5d5e693d86205eab2fa5612f720b62960eb29
FHGDev <gideon.lingle14@gmail.com>
2017-12-13 12:19:04 -0600

    Create listservers.js

---
+++ commands/listservers.js
@@ -0,0 +1,9 @@
+module.exports.run = (bot, message, args) => {
+  message.channel.send(`${bot.guilds.array().length} is the number of servers I'm on.`)
+}
+
+module.exports.help = {
+  name: "",
+  usage: ``,
+  information: ""
+} 
