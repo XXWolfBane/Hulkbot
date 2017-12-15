d64ef05154f3ba7fba84ddbe62d2a163e7837cd3
FHGDev <gideon.lingle14@gmail.com>
2017-12-15 17:50:50 -0600

    Create youtube.js

---
+++ commands/youtube.js
@@ -0,0 +1,10 @@
+module.exports.run = (bot, message, args) => {
+  message.channel.send("Hulk's YouTube channel is https://www.youtube.com/c/FreakingHulkGaming")
+  message.channel.send("This command was made on the FreakingHulk Gaming livestream.")
+}
+
+module.exports.help = {
+  name: "yt",
+  usage: ``,
+  information: ""
+}
