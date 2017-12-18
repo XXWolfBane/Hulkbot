c61977b4bbd78ec8eba3b93fa6438d4c3e434424
FHGDev <gideon.lingle14@gmail.com>
2017-12-18 11:20:00 -0600

    Update timer.js

--- commands/timer.js
+++ commands/timer.js
@@ -1,11 +1,18 @@
 module.exports.run = (bot, message, args) => {
   let channel = message.channel
+  var counting = "true"
   var num = 10
   message.channel.send("Counting down from 10!")
+  if (counting == "true") {
   setInterval(() => {
-    num = num - 1
-    channel.send(num)
+    if (num == 1) {
+      channel.send("BEEEEEP! Time is up.").then(counting = "false")
+    } else {
+      channel.send(num)
+      num = num - 1
+    }
   }, 1)
+  }
 }
 
 module.exports.help = {
