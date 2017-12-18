ab9f297f7e821760ab15a932a8985ae24ce01f8c
FHGDev <gideon.lingle14@gmail.com>
2017-12-18 11:30:16 -0600

    Update question.js

--- commands/question.js
+++ commands/question.js
@@ -1,10 +1,12 @@
 module.exports.run = (bot, message, args) => {
   const result = Math.round(Math.random())
-  if (result) {
+  if (message.content.startsWith("h!question")) {
+    if (result) {
     message.channel.send("Yes!")
   } else {
     message.channel.send("No.")
   }
+}
 }
 
 module.exports.help = {
