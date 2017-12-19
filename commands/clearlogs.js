0dea558575e6558e28673cb1c5a9f9f364e43317
FHGDev <gideon.lingle14@gmail.com>
2017-12-19 12:31:57 -0600

    Update clearlogs.js

--- commands/clearlogs.js
+++ commands/clearlogs.js
@@ -3,14 +3,11 @@ const prefix = process.env.prefix
 
 module.exports.run = (bot, message, args) => {
   if (message.channel.id == config.logid) {
-    if (message.content == prefix + "clearlogs") {
       message.channel.bulkDelete(100).catch(console.err)
       message.channel.send("Successfully cleared the logs.").then(m => m.delete(5000))
+    } else {
+        console.log("Failed to clear logs. :-(")
     }
-    else {
-      console.log("Failed to clear logs. :-(")
-    }
-  }
 }
 
 module.exports.help = {
