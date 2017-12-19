d76f8abc57239e78437daea960c4330a9e77d02b
FHGDev <gideon.lingle14@gmail.com>
2017-12-19 12:16:39 -0600

    Update invisi.js

--- commands/invisi.js
+++ commands/invisi.js
@@ -1,5 +1,11 @@
+const oid = process.env.oid
+
 module.exports.run = (bot, message, args) => {
+  if (message.author.id == oid) {
   bot.user.setStatus(`invisible`)
+} else {
+  message.channel.send("Nope!")
+}
 }
 
 module.exports.help = {
