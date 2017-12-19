74adeba7ee97c1271f8e9f5261b5fbf6f75df646
FHGDev <gideon.lingle14@gmail.com>
2017-12-19 13:13:16 -0600

    Update debug.js

--- commands/debug.js
+++ commands/debug.js
@@ -22,9 +22,8 @@ module.exports.run = (bot, message, args) => {
 🔎 **I know:**\n -   ${bot.guilds.array().length} Guilds\n -   ${bot.channels.array().length} Channels\n -   ${bot.users.array().length} Users\n
 ⌛ **Uptime:** ${Math.round(bot.uptime / (1000 * 60 * 60))} hours, ${Math.round(bot.uptime / (1000 * 60)) % 60}  minutes, ${Math.round(bot.uptime / 1000) % 60} seconds.\n
 🔊 **Status Updates:** [#${config.statues.length}]\n      ${config.statues.join("\n   ")}\n
-📄 **Whitelist Guild ids:** [#${config.whitelistedServers.length}]\n      ${config.whitelistedServers.join("\n      ")}\n
 📝 **Guilds I Know:** [#${bot.guilds.array().length}]\n${guild_list}`)
-  message.channel.send({embed}).then(m => m.delete(35000))
+  message.channel.send({embed})
 }
 
 module.exports.help = {
