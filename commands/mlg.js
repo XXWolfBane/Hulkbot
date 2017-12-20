9ce50ddf5ecbf90acb90974dc22c98532d80d207
FHGDev <gideon.lingle14@gmail.com>
2017-12-19 19:19:19 -0600

    Create mlg.js

---
+++ commands/mlg.js
@@ -0,0 +1,7 @@
+module.exports.run = (bot, msg) => {
+   message.channel.send("https://cdn.discordapp.com/attachments/356178662837452801/392847256484708352/clubpenguin.png")
+}
+
+module.exports.help = {
+  name: "mlg"
+}
