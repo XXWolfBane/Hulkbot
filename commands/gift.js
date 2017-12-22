02c2c1ef3016ad2c718b9235f121abac5e0c98f3
FHGDev <gideon.lingle14@gmail.com>
2017-12-22 15:43:11 -0600

    Create gift.js

---
+++ commands/gift.js
@@ -0,0 +1,12 @@
+module.exports.run = (bot, message, args, discord) => {
+  let channel = message.channel
+  const embed = new discord.RichEmbed()
+    .setTitle("Here's a gif from the bot owner! Merry Christmas!")
+    .setDescription("https://media.giphy.com/media/gNke2UrUTopOg/giphy.gif")
+  
+  channel.send({embed})
+}
+
+module.exports.help = {
+  name: "gift"
+}
