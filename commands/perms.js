529fb6d496cc6a379cb02661c82636fd63271822
FHGDev <gideon.lingle14@gmail.com>
2017-12-22 18:31:15 -0600

    Update perms.js

--- commands/perms.js
+++ commands/perms.js
@@ -1,8 +1,24 @@
+const BMA = require('../config.json').BMA
+const BCO = require('../config.json').BCO
+const OID = process.env.oid
+
+const mainroles = [BMA, BCO, OID]
+
 module.exports.run = (bot, message, args) => {
-  message.author.send("No permissions yet. Contact <@242734840829575169> for details.")
+  if (message.author.id == BCO) {
+  message.author.send(`Your permissions are Co-Owner level! You have full permissions.`)
+  }
+  if (message.author.id == BMA) {
+    message.author.send(`Your permissions are Main Admin level! You have full permissions.`)
+  }
+  if (message.author.id == process.env.oid) {
+    message.author.send(`You are the owner! Hello my Creator!`)
+  }
+  if (!message.author.id == mainroles) {
+    message.author.send(`You have the everyone permissions.`)
+  }
 }
 
 module.exports.help = {
-  name: "perms",
-
+  name: "perms"
 }
