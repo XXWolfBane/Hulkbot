8ffb18bdd11b4bbbd4378e9e6cb99337c82bd2f6
FHGDev <gideon.lingle14@gmail.com>
2018-01-01 16:07:32 -0600

    Update online.js

--- commands/online.js
+++ commands/online.js
@@ -1,5 +1,17 @@
+const discord = require('discord.js')
+
 module.exports.run = (bot, message, args) => {
+  if (message.author.id == process.env.oid) {
   bot.user.setStatus(`online`)  
+  let embed = new discord.RichEmbed()
+    .setTitle("Status Changer")
+    .setDescription("Successfully set the bot status to Online!")
+    .setColor("RED")
+    .setThumbnail(bot.user.avatarURL)
+    .setFooter(`This command was used at ${new Date()}`)
+ } else {
+      message.channel.send("Nope!") 
+  }
 }
 
 module.exports.help = {
