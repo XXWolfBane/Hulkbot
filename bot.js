284fd92c28b4677395b311483aa1d3d4eef1ad74
FHGDev <gideon.lingle14@gmail.com>
2017-12-31 21:34:53 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -53,8 +53,8 @@ bot.on("ready", () => {
     console.log(`[SERVER] [${guild.memberCount}] ${guild.name} (${guild.id}) | Joined: ${guild.joinedAt.toString()}`)
     
     // send to all servers v v v 
-     //guild.channels.find('name', 'general').send(`Hulkbot Public Announcement: NEW COMMAND! You can now redeem codes for the bot, and get the SuperPatron role on my discord! Go to my twitter page to redeem the code! All you have to do is say 'h!redeem (code here)'`)
-     //guild.channels.find('name', 'chat').send(`Hulkbot Public Announcement: NEW COMMAND! You can now redeem codes for the bot, and get the SuperPatron role on my discord! Go to my twitter page to redeem the code! All you have to do is say 'h!redeem (code here)'`)
+     guild.channels.find('name', 'general').send(`Happy new year from the owner!`)
+     guild.channels.find('name', 'chat').send(`Happy new year from the owner!`)
   })
 });
  
