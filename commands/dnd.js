01820e1a4210040baee481189dd0e5e849f6e83d
FHGDev <gideon.lingle14@gmail.com>
2018-01-01 15:04:34 -0600

    Update dnd.js

--- commands/dnd.js
+++ commands/dnd.js
@@ -1,8 +1,15 @@
 const oid = process.env.oid
+const discord = require('discord.js')
 
 module.exports.run = (bot, message, args) => {
   if (message.author.id == oid) {
   bot.user.setStatus(`dnd`)
+  let embed = new discord.RichEmbed()
+    .setTitle("Status Changer")
+    .setThumbnail(bot.user.avatarURL)
+    .setDescription("Successfully set the bot status to Do Not Disturb!")
+    
+  message.channel.send({ embed })
   } else {
     message.channel.send("Nope!")
   }
