de497f861a50f053dce79ba89db19a4dc30a6573
FHGDev <gideon.lingle14@gmail.com>
2018-01-01 16:57:07 -0600

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -2,7 +2,7 @@ const pkg = require('../package.json')
 
 module.exports.run = (bot, message, args) => {
   var msgArray = []; 
-  msgArray.push(`Hello, I'm ${bot.user.username} version ${pkg.version}, a Discord bot, running on NodeJS Version 8.`);
+  msgArray.push(`Hello, I'm ${bot.user.username} version ${pkg.version}, a Discord bot running on NodeJS Version 8.`);
   msgArray.push(`My create date is January of 2017`)
   msgArray.push(`I'm currently running on Discord.js version 12.0.0 which utilizes the latest version of Discord.js, an *unofficial* Discord libary by hydrabolt.`);
   msgArray.push(`The developers of my source code are <@242734840829575169> and <@140487710727995392>.`);
