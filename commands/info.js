ba664701b2ec24e07f7740a0dea00c2d05a7ec9a
FHGDev <gideon.lingle14@gmail.com>
2018-01-01 16:55:51 -0600

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -2,9 +2,9 @@ const pkg = require('../package.json')
 
 module.exports.run = (bot, message, args) => {
   var msgArray = []; 
-  msgArray.push(`Hello, I'm ${bot.user.username}, a Discord bot.`);
+  msgArray.push(`Hello, I'm ${bot.user.username} version ${pkg.version}, a Discord bot, running on NodeJS Version 8.`);
   msgArray.push(`My create date is January of 2017`)
-  msgArray.push(`I'm currently running on Discord.js version ${pkg.version} which utilizes the latest version of Discord.js, an *unofficial* Discord libary by hydrabolt.`);
+  msgArray.push(`I'm currently running on Discord.js version 12.0.0 which utilizes the latest version of Discord.js, an *unofficial* Discord libary by hydrabolt.`);
   msgArray.push(`The developers of my source code are <@242734840829575169> and <@140487710727995392>.`);
   msgArray.push(`To see what I can do, use h!help`);
   msgArray.push(`My invocation method is using prefixes, currently, I only respond to messages beginning with h!`);
