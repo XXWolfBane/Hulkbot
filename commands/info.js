2318c8a8d1f47dae809ab6ec7036b639f2f2c2f2
FHGDev <gideon.lingle14@gmail.com>
2018-01-01 16:46:33 -0600

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -1,6 +1,7 @@
 module.exports.run = (bot, message, args) => {
-  var msgArray = [];
+  var msgArray = []; 
   msgArray.push(`Hello, I'm ${bot.user.username}, a Discord bot.`);
+  msgArray.push(`My create date is ${bot.OAuth2Application.createdTimestamp}`)
   msgArray.push(`I'm currently running on Discord.js version 11.2.1, which utilizes the latest version of Discord.js, an *unofficial* Discord libary by hydrabolt.`);
   msgArray.push(`The developers of my source code are <@242734840829575169> and <@140487710727995392>.`);
   msgArray.push(`To see what I can do, use h!help`);
