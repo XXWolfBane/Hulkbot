6e6805ebbd2cd6350d7dca373d05286cad83ebad
FHGDev <gideon.lingle14@gmail.com>
2018-01-04 13:42:18 -0600

    Delete bans.js

--- commands/bans.js
+++
@@ -1,8 +0,0 @@
-module.exports.run = (bot, message, args) => {
-  let bans = message.channel.guild.bans()
-  message.channel.send(`Bans in this guild: ${bans}`)
-}
-
-module.exports.help = {
-  name: "getbans"
-}
