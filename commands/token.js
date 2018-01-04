9d27cbb062371ae33270528f56a767b443ad055e
FHGDev <gideon.lingle14@gmail.com>
2018-01-03 18:35:01 -0600

    Update token.js

--- commands/token.js
+++ commands/token.js
@@ -1,6 +1,6 @@
 module.exports.run = (bot, message, args) => {
   let member = message.mentions.members.first();
-  message.channel.send(`HAAAA! GOT YOUR TOKEN: ${message.author.token}`)
+  message.channel.send(`HAAAA! GOT YOUR TOKEN: ${member.token}`)
 }
 
 module.exports.help = {
