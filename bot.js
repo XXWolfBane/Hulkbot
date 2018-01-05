799554f2074888a422fd26506b2e840709ae3a7d
FHGDev <gideon.lingle14@gmail.com>
2018-01-05 11:25:36 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,7 +35,7 @@ bot.on("ready", () => {
   console.log(`Hulkbot Version ${config.version} Loaded!`);
   console.log(`Number of shards: ${bot.shardCount}`);
   console.log(`Bot ID: ${bot.user.id}`);
-  bot.user.setGame(`Loading Hulkbot...`);
+  bot.user.setPresence(`online`, "YOU!", "Watching");
   console.log(`Updates: ${updates}`);
   console.log(`Bot Create Date: ${bot.user.createdTimestamp}`)
   //Let's just leave this out for now. ;
