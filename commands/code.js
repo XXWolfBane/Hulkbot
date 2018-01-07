665610052f72451c83074b1995c718d35f2673bc
FHGDev <gideon.lingle14@gmail.com>
2018-01-06 19:23:14 -0600

    Update code.js

--- commands/code.js
+++ commands/code.js
@@ -2,7 +2,7 @@ const code = require("../codes.json").code
 
 module.exports.run = (bot, message, args) => {
   if (message.content == "h!redeem " + code) {
-    message.channel.send("Code Redeemed! Join this discord to get the superpatron role. https://discord.gg/qEFNkxB")
+    message.channel.send("Code Redeemed! Join this discord to get the superpatron role. https://discord.gg/qEFNkxB").then(m => m.delete(5000))
   } else {
     message.channel.send("Sorry, incorrect code.")
   }
