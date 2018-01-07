d3a9f829aca80f1f3e6e7589549e522ba51ea16c
FHGDev <gideon.lingle14@gmail.com>
2018-01-06 20:04:34 -0600

    Update idle.js

--- commands/idle.js
+++ commands/idle.js
@@ -1,8 +1,15 @@
 const oid = process.env.oid
+const discord = require('discord.js')
 
 module.exports.run = (bot, message, args) => {
   if (message.author.id == oid) {
   bot.user.setStatus(`idle`)
+    let embed = new discord.RichEmbed()
+    .setTitle("Status Changer")
+    .setDescription("Successfully set the bot status to Idle!")
+    .setThumbnail(bot.user.avatarURL)
+    .setColor("YELLOW")
+    .setFooter(`This command was at ${new Date()}`)
   } else {
     message.channel.send("Nope!")
   }
