8eb0ec246166fe2961eb8303a4a7e26c95f5e0de
FHGDev <gideon.lingle14@gmail.com>
2018-01-06 20:06:16 -0600

    Update invisi.js

--- commands/invisi.js
+++ commands/invisi.js
@@ -1,8 +1,14 @@
 const oid = process.env.oid
+const discord = require('discord.js')
 
 module.exports.run = (bot, message, args) => {
   if (message.author.id == oid) {
   bot.user.setStatus(`invisible`)
+    let embed = new discord.RichEmbed()
+    .setTitle("Status Changer")
+    .setDescription("Successfully set the bot status to Invisible!")
+    .setColor("GREY")
+    .setFooter(`This command was used at ${new Date}`)
 } else {
   message.channel.send("Nope!")
 }
