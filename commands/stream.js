a2cb37bfbac34d4c68fabccf191ea7a2194eb30e
FHGDev <gideon.lingle14@gmail.com>
2018-01-08 11:45:19 -0600

    Updated Stream Link

--- commands/stream.js
+++ commands/stream.js
@@ -1,7 +1,7 @@
 module.exports.run = (bot, message, args) => {
   let channel = message.channel
   
-  channel.send("Hulk's latest stream is https://www.youtube.com/c/FreakingHulkGaming/live. I'll fetch weekly updates for ya.")
+  channel.send("Hulk's latest stream is https://www.youtube.com/watch?v=UUCvh-tAMFY. I'll fetch weekly updates for ya.")
 }
 
 module.exports.help = {
