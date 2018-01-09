0655cbc51c3f6aec5adfea2a73abc11126db613b
FHGDev <gideon.lingle14@gmail.com>
2018-01-09 10:27:33 -0600

    Delete joinserver.js

--- commands/joinserver.js
+++
@@ -1,7 +0,0 @@
-module.exports.run = (bot, message, args) => {
-  message.channel.send("Use this to join the help server: https://discord.gg/XvMA2rJ")
-}
-
-module.exports.help = {
-  name: "joinserver"
-}
