51b86dd5e85d73c5cad2b59734d4b43612c8aa39
FHGDev <gideon.lingle14@gmail.com>
2018-01-09 10:32:19 -0600

    Delete mlg.js

--- commands/mlg.js
+++
@@ -1,7 +0,0 @@
-module.exports.run = (bot, msg) => {
-   msg.channel.send("https://cdn.discordapp.com/attachments/356178662837452801/392847256484708352/clubpenguin.png")
-}
-
-module.exports.help = {
-  name: "mlg"
-}
