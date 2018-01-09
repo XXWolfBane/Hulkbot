da9b889fffede5c7eeca3cb100edd1e45e811f0e
FHGDev <gideon.lingle14@gmail.com>
2018-01-09 10:34:35 -0600

    Delete ping.js

--- commands/ping.js
+++
@@ -1,9 +0,0 @@
-module.exports.run = (bot, message, args) => {
-  message.channel.send("PONG! :ping_pong: My ping is " + bot.ping + "ms!")
-}
-
-module.exports.help = {
-  name: "ping",
-  usage: ``,
-  information: ""
-}
