133610623c6480557b8013b30a0d662939d2268b
FHGDev <gideon.lingle14@gmail.com>
2018-01-09 10:29:15 -0600

    Delete listservers.js

--- commands/listservers.js
+++
@@ -1,9 +0,0 @@
-module.exports.run = (bot, message, args) => {
-  message.channel.send(`I am in ${bot.guilds.array().length} servers. I know ${bot.users.array().length} users, and ${bot.channels.array().length} channels.`)
-}
-
-module.exports.help = {
-  name: "listservers",
-  usage: ``,
-  information: ""
-} 
