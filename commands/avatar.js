28026e4d2ead04e969bc794a44fb5e3a6c75fc4e
FHGDev <gideon.lingle14@gmail.com>
2018-01-09 09:49:02 -0600

    Delete avatar.js

--- commands/avatar.js
+++
@@ -1,9 +0,0 @@
-module.exports.run = (bot, message, args) => {
-    message.channel.send(`Avatar:\n${message.author.avatarURL}`)
-}
-
-module.exports.help = {
-  name: "avatar",
-  usage: ``,
-  information: ""
-}
