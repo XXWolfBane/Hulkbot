e83f7d3fcf7245175132f06aac23f38773218351
FHGDev <gideon.lingle14@gmail.com>
2018-01-09 09:49:15 -0600

    Delete bork.js

--- commands/bork.js
+++
@@ -1,8 +0,0 @@
-module.exports.run = (bot, message, args) => {
-	let member = message.mentions.members.first();
-	message.channel.send(`<@${member.id}> was borked by <@${message.author.id}>`);
-}
-
-module.exports.help = {
-	name: "bork",
-}
