9d252105008e98c3e28c30b185bc89d350f98284
FHGDev <gideon.lingle14@gmail.com>
2018-01-09 10:15:55 -0600

    Delete invite.js

--- commands/invite.js
+++
@@ -1,7 +0,0 @@
-module.exports.run = (bot, message, args) => {
-  message.channel.send("I can't use invite links. :frowning2: Here's an OAuth2 link instead! https://discordapp.com/oauth2/authorize?&client_id=294194506113220608&scope=bot&permissions=0")
-}
-
-module.exports.help = {
-  name: "invite",
-}
