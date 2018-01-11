c728a99c307135db569b52819463ddc17815b618
FHGDev <gideon.lingle14@gmail.com>
2018-01-11 10:41:40 -0600

    Update contact.js

--- commands/contact.js
+++ commands/contact.js
@@ -1,10 +1,10 @@
 let oid = process.env.oid
 
 module.exports.run = (bot, message, args) => {
-    var message = args.join(' ')
+    var mes = args.join(' ')
     var guild = bot.guilds.find('name', 'Hulkbot Base (and more)')
     let owner = guild.members.find('id', oid)
-    owner.send(message)
+    owner.send(`${mes}\n\n(This message was sent by ${message.author.username}.`)
 }
 
 module.exports.help = {
