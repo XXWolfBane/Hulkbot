53eaa3a591fd708f60fec88191546c642122d4a7
FHGDev <gideon.lingle14@gmail.com>
2018-01-13 16:09:45 -0600

    Update BanRemove.js

--- events/BanRemove.js
+++ events/BanRemove.js
@@ -1,8 +1,16 @@
 module.exports = (bot, guild, member) => {
   member.send(`${member.username}, you have now been unbanned from ${guild.name}!`)
-  let wel = guild.channels.find('name', 'guild-maintenance')
-  let log = ["logs", "audit", "log", "server-maintenance"]
-  wel.send(`${member.username} has been unbanned from ${guild.name}`)
-  let cnl = guild.channels.find('name', log)
+  let hbl = ["guild-maintenance"]
+ 
+  if (guild.channels.find('name' hbl)) {
+    let log = guild.channels.find('name' hbl)
+    log.send(`${member.username} has been unbanned from ${guild.name}`)
+  }
+ let log = ["logs", "audit", "log", "server-maintenance"]
+  if (guild.channels.find('name', log)) {
+    let cnl = guild.channels.find('name', log)
   cnl.send(`${member.username} has been unbanned from ${guild.name}`)
+  } else {
+    return;
+  }
 }
