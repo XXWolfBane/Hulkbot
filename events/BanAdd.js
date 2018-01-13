d8b81bd705c6e2657b0a0ac85cce3d4f8c274be7
FHGDev <gideon.lingle14@gmail.com>
2018-01-13 15:17:36 -0600

    Update BanAdd.js

--- events/BanAdd.js
+++ events/BanAdd.js
@@ -1,12 +1,15 @@
 const discord = require('discord.js')
 
 module.exports = (bot, guild, member) => {
+  console.log(`${member.username} was banned from ${guild.name} for ${reason}!`)
   let reason = require('../config.json').banreason
-  member.send(`${member.displayName}, you are now banned from ${guild.name} for the reason of ${reason}!`)
+  member.send(`${member.displayName}, you are now banned from ${guild.name} for the reason ${reason}!`)
   let log = guild.channels.find('name', 'logs')
   let embed = new discord.RichEmbed()
   .setTitle("Log")
-  .setDescription(`${member.displayName} was banned from ${guild.name} for the reason of ${reason}!`)
+  .setDescription(`${member.username} was banned from ${guild.name} for the reason ${reason}!`)
+  .setThumbnail(member.avatarURL)
+  .setColor("RED")
   .setFooter(`${member.displayName} was banned at ${new Date}`)
   log.send({ embed })
 }
