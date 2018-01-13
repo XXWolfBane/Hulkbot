819fe598b87526f1bb82f968588ce439e15dddc7
FHGDev <gideon.lingle14@gmail.com>
2018-01-13 13:02:50 -0600

    Create guildMemberAdd.js

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,8 @@
+module.exports = (bot, member) => {
+   console.log(`${member.displayName} just joined ${member.guild.name}`) 
+   member.send(`Welcome to ${member.guild.name}, ${member.displayName}!`)
+   const channel = member.guild.channels.find('name', 'welcome')
+   if (!channel) return;
+   
+   channel.send(`Welcome to ${member.guild.name}, <@${member.id}>`)
+}
