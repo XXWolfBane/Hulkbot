e3c2d75a021f5f461a9d128b51b2fb2cb3349c15
Fuzzy <NovalFuzzy@outlook.com>
2018-01-14 15:33:10 -0800

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,40 +1,6 @@
-module.exports.run = (bot, message, args) => {
-    message.channel.send({
-        embed: {
-            color: 3447003,
-            author: {
-                name: `${bot.user.username}`,
-                icon_url: `${bot.user.avatarURL}`
-            },
-            title: `:ping_pong: Pong!`,
-            description: `Heyo! I'm watching over**${bot.users.array().length} users** in over **${bot.guilds.array().length} servers**!`,
-        },
-    }).then((m) =>{
-        m.edit({
-            embed: {
-                color: 3447003,
-                author: {
-                    name: `${bot.user.username}`,
-                    icon_url: `${bot.user.avatarURL}`
-                },
-                title: `:ping_pong: Pong!`,
-                description: `Heyo! I'm watching over**${bot.users.array().length} users** in over **${bot.guilds.array().length} servers**!`,
-                fields: [
-                    {
-                        name: `:information_source: Latency:`,
-                        value: `\`${m.createdTimestamp - message.createdTimestamp}ms\``,
-                        inline: true
-                    },
-                    {
-                        name: `:heartpulse: Heartbeat:`,
-                        value: `\`${Math.round(bot.ping)}ms\``,
-                        inline: true
-                    }
-                ]
-            },
-        })
-    }
-    });
+  module.exports.run = (bot, message, args) => {
+    message.channel.send("PONG! :ping_pong: My ping is " + bot.ping + "ms!")
+  }
   
   module.exports.help = {
     name: "ping",
