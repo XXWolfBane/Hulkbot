1853e927b240827d2cb4f3643e4a44c34b9e438f
FHGDev <gideon.lingle14@gmail.com>
2018-01-14 16:14:11 -0600

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,5 +1,12 @@
+const discord = require('discord.js')
+
 module.exports.run = (bot, message, args) => {
-  message.channel.send("I can't use invite links. :frowning2: Here's an OAuth2 link instead! https://discordapp.com/oauth2/authorize?&client_id=294194506113220608&scope=bot&permissions=0")
+  let embed = new discord.RichEmbed()
+  .setTitle("Hulkbot Invitation")
+  .setDescription(`I can't use invite links.\nHere's an OAuth2 link instead! https://discordapp.com/api/oauth2/authorize?client_id=294194506113220608&permissions=8&redirect_uri=https%3A%2F%2Fbot.hulkbot.ml%2Fhome&response_type=code&scope=bot%20guilds`)
+  .setColor("GREEN")
+  .setFooter(`This command was used at ${new Date}`)
+  message.channel.send({ embed })
 }
 
 module.exports.help = {
