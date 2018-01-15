61cd6762cda0604bf3db22508a5bd42714e12424
RHG <huskey.rusty@gmail.com>
2018-01-14 22:10:56 -0500

    Update joinserver.js

--- commands/joinserver.js
+++ commands/joinserver.js
@@ -3,20 +3,20 @@ const discord = require('discord.js');
 module.exports.run = (bot, message, args) => {
   //message.channel.send("Use this to join the help server: https://discord.gg/XvMA2rJ")
   
-  bot.guilds.get(`356178662837452800`).channels.find("name", "announcements").createInvite({
-   temporary: false, maxAge: 500, maxUses: 1, unique: true
-  }).then(invite =>{
-    let embed = new discord.RichEmbed()
-    .setAuthor(`Hulk Inviter ✍`, bot.user.avatarURL)
-    .setThumbnail(bot.guilds.get(`356178662837452800`).iconURL)
-    .setColor("ff3333")
-    .setDescription(`Yes! we have an help server!\n\n[join](${{invite}})`)
-    .setURL(invite)
-  
+  bot.guilds.get(`356178662837452800`).channels.find("name", "general").createInvite({
+   temporary: false, maxAge: 500, maxUses: 1, unique: true})
+   .then(invite =>{
+      let embed = new discord.RichEmbed()
+      .setAuthor(`Hulk Inviter ✍`, bot.user.avatarURL)
+      .setThumbnail(bot.guilds.get(`356178662837452800`).iconURL)
+      .setColor("ff3333")
+      .setDescription(`Yes! we have an help server!\n\n[join](${{invite}})`);
+    
   message.channel.send({embed});
+    
   });
-}
+};
 
 module.exports.help = {
   name: "joinserver"
-}
+};
