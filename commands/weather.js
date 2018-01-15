5ebf75c7a544442e665ed1dff0e5db7a7646b5dd
RHG <huskey.rusty@gmail.com>
2018-01-14 23:01:18 -0500

    Update weather.js

--- commands/weather.js
+++ commands/weather.js
@@ -7,7 +7,7 @@ module.exports.run = (bot, message, args) => {
   w.find({search: args[0], degreeType: 'F'}, function(err, result) {
   if(err) console.log(err);
  
-  JSON.stringify(result, null, 2).then(location =>{
+  let location = JSON.stringify(result, null, 2)
         let embed = new discord.RichEmbed()
           .setColor(message.guild.me.displayHexColor !='#000000' ? message.guild.me.displayHexColor : "7289DA")
           .setAuthor(`:cloud: **Weather for ${location.name}**`)
@@ -15,7 +15,6 @@ module.exports.run = (bot, message, args) => {
           .setFooter(foot);
 
        message.channel.send({ embed })
-    });
   });
 }
 
