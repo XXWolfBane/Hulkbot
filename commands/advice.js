ab6c07adc43077770424183dd3f13d8b730c7c09
FHGDev <gideon.lingle14@gmail.com>
2018-01-15 11:32:52 -0600

    Create advice.js

---
+++ commands/advice.js
@@ -0,0 +1,15 @@
+module.exports.run = (bot, message, args) => {
+  require('request')('http://api.adviceslip.com/advice', function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      message.channel.send(`**Advice:**\n\n ${JSON.parse(body).slip.advice}`)
+    } else {
+      message.channel.send(`**Advice:**\n I couldn't think of any advice...`)
+    }
+  });
+}
+
+module.exports.help = {
+  name: "advice",
+  usage: ``,
+  information: "I'll give you some great advice, I'm just too kind."
+}
