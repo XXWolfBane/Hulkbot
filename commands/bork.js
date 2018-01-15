3e471a0e0a1b00c3588b6d0c28c2e3440f81d296
FHGDev <gideon.lingle14@gmail.com>
2018-01-14 18:34:54 -0600

    Update bork.js

--- commands/bork.js
+++ commands/bork.js
@@ -1,6 +1,13 @@
+const discord = require('discord.js')
+
 module.exports.run = (bot, message, args) => {
 	let member = message.mentions.members.first();
-	message.channel.send(`<@${member.id}> was borked by <@${message.author.id}>`);
+	let embed = new discord.RichEmbed()
+	.setTitle(`BORK!`)
+	.setDescription(`<${@member.id}> was borked by <@${message.author.id}>`)
+	.setThumbnail(`https://goo.gl/gFQRX3`)
+	.setColor(`RANDOM`)
+	message.channel.send({ embed });
 }
 
 module.exports.help = {
