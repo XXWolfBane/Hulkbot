5fb0a86602cd070d57721f23d0816bd0c2847319
RHG <huskey.rusty@gmail.com>
2018-01-14 22:33:22 -0500

    Update weather.js

--- commands/weather.js
+++ commands/weather.js
@@ -2,11 +2,11 @@ const weather = require('weather-js')
 
 module.exports.run = (bot, message, args) => {
   weather.find({
-    search: args[<3],
-    degreetype: args[2]
+    search: args.slice(2).join(""),
+    degreetype: "F"
     },
     function (err, result) {
-    console.log(result)
+      console.log(result)
       message.channel.send(`The weather in your area is ${result}!`)
    })
 }
