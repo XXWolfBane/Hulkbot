1007bf98b4b6e3c471b36687131e3954e1dfc63f
FHGDev <gideon.lingle14@gmail.com>
2018-01-14 19:07:01 -0600

    Update changegame.js

--- commands/changegame.js
+++ commands/changegame.js
@@ -6,7 +6,7 @@ module.exports.run = (bot, message, args) => {
  if (message.author.id == oid) {
  if (!args) return message.channel.send(`:ok_hand: Okay, I will set my playing status back to normal!`)
  var game = args.join(' ')
- bot.user.setActivity(game, {type: "PLAYING"})
+ bot.user.setActivity(`${game}`, {type: "PLAYING"})
  message.channel.send(`:ok_hand: Okay, I will set my playing status to ${game}!`)
 } else {
   message.channel.send("Nope!")
