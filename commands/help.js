0dcf55678f5ce033e58d53ffb2347efc9e725246
Fuzzy <NovalFuzzy@outlook.com>
2018-01-14 18:18:30 -0800

    Moar booti

--- commands/help.js
+++ commands/help.js
@@ -8,15 +8,15 @@ module.exports.run = (bot, message, args, suffix) => {
     {
       name: ":hammer_pick: Moderation:",
       value: `mute, unmute, kick, ban, auth, purge, perms, filteroff, and filteron.`,
-      value: `\`\`\`css\nauth = "No Usage Provided!"\nban = "Mention | User ID"\nfilteron = "No Usage Provided!"\nfilteroff = "No Usage Provided!"\nkick = "Mention | User ID"\nmute = "Mention | User ID"\nunmute = "Mention | User ID\npurge = "Amount of Msgs"\`\`\``,
+      value: `\`\`\`ini\nauth = "No Usage Provided!"\nban = "Mention | User ID"\nfilteron = "No Usage Provided!"\nfilteroff = "No Usage Provided!"\nkick = "Mention | User ID"\nmute = "Mention | User ID"\nunmute = "Mention | User ID\npurge = "Amount of Msgs"\`\`\``,
     },
     {
       name: ":robot: Bot:",
-      value: `\`\`\`css\njoinserver = "No Usage Provided!"\ninvite = "No Usage Provided!"\ninfo = "No Usage Provided!"\nping = "No Usage Provided!"\nuptime = "No Usage Provided!"\`\`\``,
+      value: `\`\`\`ini\njoinserver = "No Usage Provided!"\ninvite = "No Usage Provided!"\ninfo = "No Usage Provided!"\nping = "No Usage Provided!"\nuptime = "No Usage Provided!"\`\`\``,
     },
            {
       name: ":lollipop: Fun:",
-      value: `\`\`\`css\navatar = "Blank for youself | Mention for others"\nbork = "Mention | User ID"\ncoinflip = "No Usage Provided!"\nsay = "message"\nmyid = "No Usage Provided!"\nserverid = "No Usage Provided!"\nyomomma = "No Usage Provided!"\n\`\`\``,
+      value: `\`\`\`ini\navatar = "Blank for youself | Mention for others"\nbork = "Mention | User ID"\ncoinflip = "No Usage Provided!"\nsay = "message"\nmyid = "No Usage Provided!"\nserverid = "No Usage Provided!"\nyomomma = "No Usage Provided!"\n\`\`\``,
     },
     {
       name: "Need More Help?",
@@ -26,7 +26,7 @@ module.exports.run = (bot, message, args, suffix) => {
     if(suffix == "-o") {
       embed_fields.push({
         name: ":medal: Owner:",
-	value: `\`\`\`css\nshutdown = "No Usage Provided!"\nchangegame = "No Usage Provided!"\`\`\``,
+	value: `\`\`\`ini\nshutdown = "No Usage Provided!"\nchangegame = "No Usage Provided!"\`\`\``,
         inline: false
       });
     } else {
@@ -41,7 +41,6 @@ module.exports.run = (bot, message, args, suffix) => {
     },
     title: "Help Message!",
     url: "https://discord.gg/XvMA2rJ",
-    description: "Help has arrived! ( ͡° ͜ʖ ͡°)",
     fields: fields,
     timestamp: new Date(),
     footer: {
