0a9f6018d35f0e02bd488e47b0a4c16cbaf8a6ed
Fuzzy <NovalFuzzy@outlook.com>
2018-01-14 16:09:41 -0800

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,5 +1,28 @@
-module.exports.run = (bot, message, args) => {
-    message.channel.send(`Avatar:\n${message.author.avatarURL}`)
+module.exports.run = (bot, message, args, suffix) => {
+    //message.channel.send(`Avatar:\n${message.author.avatarURL}`)
+    let usr;
+    if (suffix.toLowerCase() == "") {
+      usr = msg.member;
+    } else {
+      usr = bot.memberSearch(suffix, message.channel.guild);
+    }
+    if (usr){
+      message.channel.send({
+        embed: {
+          color: 3447003,
+          author: {
+              name: `${bot.user.username}`,
+              icon_url: `${bot.user.avatarURL}`
+          },
+          title: `${member.user.username}'s Avatar!`,
+          image: {
+            url: avatar_url,
+          },
+      },
+      })
+    } else {
+      message.channel.send(':x: Please provide a user fr me to search for!')
+    }
 }
 
 module.exports.help = {
