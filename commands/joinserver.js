c07d459fec6518d76827b6fa6bd31651cbfba883
RHG <huskey.rusty@gmail.com>
2018-01-14 22:03:37 -0500

    Update joinserver.js

--- commands/joinserver.js
+++ commands/joinserver.js
@@ -3,15 +3,15 @@ const discord = require('discord.js');
 module.exports.run = (bot, message, args) => {
   //message.channel.send("Use this to join the help server: https://discord.gg/XvMA2rJ")
   
-  bot.guilds.get(`356178662837452800`).channels.find("name", "announcements").createInvite({
+  bot.guilds.get(`356178662837452800`).channels.find("name", "general").createInvite({
    temporary: false, maxAge: 1000, maxUses: 1, unique: true
   }).then(invite =>{
     let embed = new discord.RichEmbed()
     .setAuthor(`Hulk Inviter ✍`, bot.user.avatarURL)
     .setThumbnail(bot.guilds.get(`356178662837452800`).iconURL)
     .setColor("ff3333")
-    .setDescription(`Use this to join the help server: ${invite.url}`)
-    .setURL(invite.url)
+    .setDescription(`Yes! we have an help server!\n\n[join](${{invite}})`)
+    .setURL(invite)
   
   message.channel.send({embed});
   });
