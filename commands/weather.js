f49a4cebc2c819e1a1b57c78ad8149c21ab7ce7f
RHG <huskey.rusty@gmail.com>
2018-01-14 23:15:18 -0500

    Update weather.js

--- commands/weather.js
+++ commands/weather.js
@@ -3,21 +3,25 @@ const discord = require(`discord.js`);
 
 
 module.exports.run = (bot, message, args) => {
-  
-  w.find({search: args[0], degreeType: 'F'}, function(err, result) {
-  if(err) console.log(err);
- 
-  let weather = JSON.stringify(result, null, 2)
-  console.log(weather)
-  let embed = new discord.RichEmbed()
+  loc = message.content.substring(10)
+	if (!loc) {
+		m.channel.sendMessage("You need to supply a City!")
+		return
+	}
+	w.find({search: loc, degreeType: 'F'}, function(err, result) {
+				if (err) {
+					message.channel.sendMessage(err)
+				};
+  m.channel.sendMessage("Weather for: " + result[0].location.name + "\nTemperature: " + result[0].current.temperature + "°C\nFeels like: " + result[0].current.feelslike + "°C\n" + result[0].current.skytext + "\n" + result[0].current.humidity + "% Humidity \nWind Speed: " + result[0].current.winddisplay)
+		m.channel.sendMessage("Weather for: " + result[0].location.name + "\nTemperature: " + result[0].current.temperature + "°C\nFeels like: " + result[0].current.feelslike + "°C\n" + result[0].current.skytext + "\n" + result[0].current.humidity + "% Humidity \nWind Speed: " + result[0].current.winddisplay)
+
+    let embed = new discord.RichEmbed()
       .setColor(message.guild.me.displayHexColor !='#000000' ? message.guild.me.displayHexColor : "7289DA")
-      .setAuthor(`☁ Weather for ${weather.location}`)
-      .setDescription(`dank`)
+      .setAuthor(`☁ Weather for ${result[0].location.name}`)
+      .setDescription("\nTemperature: " + result[0].current.temperature + "°C\nFeels like: " + result[0].current.feelslike + "°C\n" + result[0].current.skytext + "\n" + result[0].current.humidity + "% Humidity \nWind Speed: " + result[0].current.winddisplay)
       .setFooter(`dank`)
       .addField(`Title`, `des`)
-    
-
-       message.channel.send({ embed })
+    message.channel.send({ embed })
   });
 }
 
