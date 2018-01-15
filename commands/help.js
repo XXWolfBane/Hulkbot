0f69faefdc8845a247358535a97a65418e7a7bdb
Fuzzy <NovalFuzzy@outlook.com>
2018-01-14 18:10:50 -0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,50 +1,55 @@
-module.exports.run = (bot, message, args) => {
-  message.channel.send({embed: {
+module.exports.run = (bot, message, args, suffix) => {
+  let fields = [
+    {
+      name: ":gear: Prefix:",
+      value: `\`h!\``,
+      inline: true
+    },
+    {
+      name: ":hammer_pick: Moderation:",
+      value: `mute, unmute, kick, ban, auth, purge, perms, filteroff, and filteron.`,
+      value: `\`\`\`css\nauth = "No Usage Provided!"\nban = "Mention | User ID"\nfilteron = "No Usage Provided!"\nfilteroff = "No Usage Provided!"\nkick = "Mention | User ID"\nmute = "Mention | User ID"\nunmute = "Mention | User ID\npurge = "Amount of Msgs"\`\`\``,
+    },
+    {
+      name: ":robot: Bot:",
+      value: `\`\`\`css\njoinserver = "No Usage Provided!"\ninvite = "No Usage Provided!"\ninfo = "No Usage Provided!"\nping = "No Usage Provided!"\nuptime = "No Usage Provided!"\`\`\``,
+    },
+           {
+      name: ":lollipop: Fun:",
+      value: `\`\`\`css\navatar = "Blank for youself | Mention for others"\nbork = "Mention | User ID"\ncoinflip = "No Usage Provided!"\nsay = "message"\nmyid = "No Usage Provided!"\nserverid = "No Usage Provided!"\nyomomma = "No Usage Provided!"\n\`\`\``,
+    },
+    {
+      name: "Need More Help?",
+      value: `[Click Here!](https://bot.hulkbot.ml/commands)`,
+    }
+  ];
+    if(suffix == "-o") {
+      embed_fields.push({
+        name: ":medal: Owner:",
+				value: `\`\`\`css\nshutdown = "No Usage Provided!"\nchangegame = "No Usage Provided!"\`\`\``,
+        inline: true
+      });
+    } else {
+      null;
+    };
+  message.channel.send({
+    embed: {
     color: 3447003,
     author: {
       name: bot.user.username,
       icon_url: bot.user.avatarURL
     },
-    title: "Help",
+    title: "Help Message!",
     url: "https://discord.gg/XvMA2rJ",
-    description: "You asked for help, here it is.",
-    fields: [
-      {
-        name: "Prefix",
-        value: "h!"
-      },
-      {
-        name: "Owner Only",
-        value: "shutdown and changegame."
-      },
-      {
-        name: "Utility :tools:",
-        value: "mute, unmute, kick, ban, auth, purge, perms, filteroff, and filteron."
-      },
-      {
-        name: "Bot Status :robot:",
-        value: "uptime and ping."
-      },
-      {
-        name: "Adding the bot, and more help.",
-        value: "joinserver, invite, info, and contact."
-      },
-             {
-        name: "Fun",
-        value: "coinflip, say, myid, avatar, serverid, bork, and yomomma."
-      },
-      {
-        name: "More",
-        value: "Additional commands at https://bot.hulkbot.ml/commands"
-      }
-    ],
+    description: "Help has arrived! ( ͡° ͜ʖ ͡°)",
+    fields: fields,
     timestamp: new Date(),
     footer: {
       icon_url: bot.user.avatarURL,
       text: ""
     }
   }
-});
+})
 }
 
 module.exports.help = {
