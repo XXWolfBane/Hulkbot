aa214cb6c67bd6cd664708980bdaa69c93561a82
FHGDev <gideon.lingle14@gmail.com>
2018-01-15 13:00:17 -0600

    Update code.js

--- commands/code.js
+++ commands/code.js
@@ -1,8 +1,15 @@
 const code = require("../codes.json").code
 
-module.exports.run = (bot, message, args) => {
+module.exports.run = (bot, message, args, discord) => {
   if (message.content == "h!redeem " + code) {
-    message.channel.send("Code Redeemed! Join this discord to get the superpatron role. https://discord.gg/qEFNkxB").then(m => m.delete(5000))
+    let em = new discord.RichEmbed()
+    .setTitle(`${bot.user.displayName} Code Redemption`)
+    .setDescription(`Correct Code! Use this invite to go to the Hulkbot Base discord where you'll get the SuperPatron role, and more!`)
+    .setField(`[Invite](https://discord.gg/qEFNkxB "Click to go to the Hulkbot Base!")`)
+    .setAuthor(`${bot.user.avatarURL} ${bot.user.displayName}`)
+    .setColor(`BLUE`)
+    .setFooter(new Date())
+    message.channel.send({ embed })
   } else {
     message.channel.send("Sorry, incorrect code.")
   }
