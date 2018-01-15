e6d614a28d933a4e8a181e029b5116574c7d4da1
FHGDev <gideon.lingle14@gmail.com>
2018-01-15 16:21:19 -0600

    Update cleverbot.js

--- events/cleverbot.js
+++ events/cleverbot.js
@@ -1,15 +1,18 @@
 var cleverbot = require('cleverbot.io'),
 cb = new cleverbot("sMNApmkOjMlZRlPZ", "gskxw3JBqEVGIAboBjOnvyTf8awM1MbS")
 
-module.exports = (bot, message) => {
+module.exports = (bot, message, bot.commands) => {
   if (message.author.bot) return;
+  let cmd = bot.commands
   cb.setNick("Hulkbot")
   cb.create(function (err, session) {
+    if (cmd) {
     cb.ask(message.content, function (err, response) {
       message.channel.send(response)
       console.log(`${message.author.username} asked ${message.content}, and Hulkbot responded with ${response}!`)
       if (err) {
         console.error
+        }
       }
     })
   })
