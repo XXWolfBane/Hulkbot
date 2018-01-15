dabca9e4a4cf2e70f736ba72a77736410d865d66
FHGDev <gideon.lingle14@gmail.com>
2018-01-14 21:12:39 -0600

    Update joinserver.js

--- commands/joinserver.js
+++ commands/joinserver.js
@@ -10,7 +10,7 @@ module.exports.run = (bot, message, args) => {
       .setAuthor(`Hulk Inviter ✍`, bot.user.avatarURL)
       .setThumbnail(bot.guilds.get(`356178662837452800`).iconURL)
       .setColor("ff3333")
-      .setDescription(`Yes! we have an help server!\n\n[join](${{invite}})`);
+      .setDescription(`Yes! We have a help server!\n\nJoin here: ${invite}`);
     
   message.channel.send({embed});
     
