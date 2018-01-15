4731eb9b1187ed8def1a85e3c72e2080d8827f3c
RHG <huskey.rusty@gmail.com>
2018-01-14 21:33:18 -0500

    Update joinserver.js

--- commands/joinserver.js
+++ commands/joinserver.js
@@ -1,5 +1,24 @@
+const discord = require('discord.js');
+
 module.exports.run = (bot, message, args) => {
-  message.channel.send("Use this to join the help server: https://discord.gg/XvMA2rJ")
+  //message.channel.send("Use this to join the help server: https://discord.gg/XvMA2rJ")
+  
+  bot.guilds.get(`356178662837452800`).channels.find("name", "general").createInvite({
+  "temporary": false,
+   "maxAge: 0,
+   "maxUses": 1,
+   "unique": true
+  },"Commands").then(invite =>{
+    let embed = new discord.RichEmbed()
+    .setTimestamp()
+    .setAuthor(`Hulk Inviter ✍`, bot.user.avatarURL)
+    .setThumbnail(bot.guilds.get(`356178662837452800`).iconURL)
+    .setColor("ff3333")
+    .setDescription(`Use this to join the help server: ${invite}`)
+    .setURL(invite)
+  
+  bot.channels.get(logchannel).send({embed});
+  });
 }
 
 module.exports.help = {
