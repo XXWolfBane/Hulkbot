e738ca8f2b99e4f6d1113354370d3ccc4f842ca2
FHGDev <gideon.lingle14@gmail.com>
2018-01-14 18:58:29 -0600

    Update bork.js

--- commands/bork.js
+++ commands/bork.js
@@ -5,7 +5,7 @@ module.exports.run = (bot, message, args) => {
 	let embed = new discord.RichEmbed()
 	.setTitle(`BORK!`)
 	.setImage(`https://orig00.deviantart.net/3bd3/f/2015/141/0/5/bork_by_chiibe-d8u6uv3.gif`)
-	.setDescription(`<@${member.id}> was borked by <@${message.author.id}>`)
+	.setDescription(`( ͡° ͜ʖ ͡°) <@${member.id}> was borked by <@${message.author.id}>`)
 	.setThumbnail(bot.user.avatarURL)
 	.setColor(`RANDOM`)
 	message.channel.send({ embed });
