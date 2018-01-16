dd5ffa44c7fa85a5e4eb8868373e46b7e957f59b
FHGDev <gideon.lingle14@gmail.com>
2018-01-15 20:25:17 -0600

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -2,15 +2,15 @@ module.exports.run = (bot, message, args, discord) => {
   let embed = new discord.RichEmbed()
   .setTitle('Hulkbot Status')
   .setColor(`RANDOM`)
-  .setDescription(`Bot Status: ${bot.user.status}`)
-  .addField(`Bot Servercount: ${bot.guilds.array().length}`)
-  .addField(`Bot Usercount: ${bot.users.array().length}`)
-  .addField(`Bot Channelcount: ${bot.channels.array().length}`)
-  .addField(`Memory Usage: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`)
-  .addField(`Discord.js Version 11.3.0`)
-  .addField(`Node Version 8.9.4`)
-  .addField(`Source Code: [GitHub](https://github.com/FHGDev/JSHulkbot)`)
-  .addField(`Hulkbot Website: [Website](https://bot.hulkbot.ml/home)`)
+  .setDescription(`Bot Status: ${bot.user.status}
+  Bot Servercount: ${bot.guilds.array().length}
+  Bot Usercount: ${bot.users.array().length}
+  Bot Channelcount: ${bot.channels.array().length}
+  Memory Usage: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
+  Discord.js Version 11.3.0
+  Node Version 8.9.4
+  Source Code: [GitHub](https://github.com/FHGDev/JSHulkbot)
+  Hulkbot Website: [Website](https://bot.hulkbot.ml/home)`)
   .setFooter(new Date())
   
   message.channel.send({ embed })
