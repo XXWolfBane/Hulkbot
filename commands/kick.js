029ebf2240cb81dcc65a4c91a63345e93bb50286
FHGDev <gideon.lingle14@gmail.com>
2018-01-16 16:22:39 -0600

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -9,7 +9,6 @@ module.exports.run = (bot, message, args, discord) => {
  .setDescription(`${member.displayName} was successfully kicked.`)
  
   if (message.member.hasPermission("KICK_MEMBERS")) {
-   
     member.kick(reason).then(channel.send({ embed }))
   } else {
     message.channel.send("You don't have permission to kick people. If there's someone who needs to be kicked, contact an administrator or a moderator.")
