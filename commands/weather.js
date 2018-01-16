6df3e561c52ab74bde29534381d1710b05c67109
RHG <huskey.rusty@gmail.com>
2018-01-15 21:32:53 -0500

    Update weather.js

--- commands/weather.js
+++ commands/weather.js
@@ -6,23 +6,28 @@ module.exports.run = (bot, message, args) => {
 	loc = message.content.substring(10)
 	if (!loc) {
 		m.channel.sendMessage("You need to supply a City!")
-		return
+		return;
 	}
 	w.find({search: loc, degreeType: 'F'}, function(err, result) {
 	if (err) {
 		message.channel.sendMessage(err)
-	};
+     };
+     let area = result[0];
 
     	let embed = new discord.RichEmbed()
-     	 .setColor(message.guild.me.displayHexColor !='#000000' ? message.guild.me.displayHexColor : "7289DA")
-     	 .setAuthor(`☁ Weather for ${result[0].location.name}`)
-      	 .setDescription("\nTemperature: " + result[0].current.temperature + "°C\nFeels like: " + result[0].current.feelslike + "°C\n" + result[0].current.skytext + "\n" + result[0].current.humidity + "% Humidity \nWind Speed: " + result[0].current.winddisplay)
-    	 .setFooter(`dank`)
-   	 .addField(`Title`, `des`)
-   	message.channel.send({ embed })
+      .setColor(message.guild.me.displayHexColor !='#000000' ? message.guild.me.displayHexColor : "7289DA")
+      .setAuthor(`☁ Weather for ${area.location.name}`)
+      .setDescription(`
+         **Temperature:** ${area.current.temperature}°F
+         **Feels Like:** ${area.current.feelslike}°F
+         **Clouds:** ${area.current.skytext}
+         **Humidity:** ${area.current.humidity}%
+         **Wind Speed:** ${area.current.winddisplay}
+         `);
+   	message.channel.send({ embed });
   });
-}
+};
 
 module.exports.help = {
   name: "weather"
-}
+};
