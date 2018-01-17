0fe733cb41e04bc158d8875ac482cfe3b06abbec
FHGDev <gideon.lingle14@gmail.com>
2018-01-16 18:20:58 -0600

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,5 +1,6 @@
 const config = require('../config.json')
 const prefix = config.prefix
+
 module.exports.run = (bot, message, args, discord) => {
  let member = message.mentions.members.first();
  let channel = message.channel
@@ -9,6 +10,7 @@ module.exports.run = (bot, message, args, discord) => {
  .setDescription(`${member.displayName} was successfully kicked.`)
  
   if (message.member.hasPermission("KICK_MEMBERS")) {
+   
     member.kick(reason).then(channel.send({ embed }))
   } else {
     message.channel.send("You don't have permission to kick people. If there's someone who needs to be kicked, contact an administrator or a moderator.")
