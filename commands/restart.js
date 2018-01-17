93e1168180b4ce5774448950d9abcb6c5836c07f
FHGDev <gideon.lingle14@gmail.com>
2018-01-17 11:40:14 -0600

    Update restart.js

--- commands/restart.js
+++ commands/restart.js
@@ -1,6 +1,14 @@
 const config = require('../config.json')
 
 module.exports.run = (bot, message, args) => {
+  const reboot = new Promise(resolve => { // reboot; kill bot and start it again
+  bot.kill('SIGTERM');
+  bot = spawn('node', ['bot'], {
+    cwd: path.join(__dirname, 'bot'),
+    env: process.env
+  });
+  resolve();
+});
   if (message.author.id == config.ownerid) {
     message.channel.send(":wave: Hulkbot will now restart.")
     reboot()
