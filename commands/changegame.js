ddb54055a3dbeb378b3107fb8005b3e55923ec13
FHGDev <gideon.lingle14@gmail.com>
2018-01-20 11:17:38 -0600

    Update changegame.js

--- commands/changegame.js
+++ commands/changegame.js
@@ -1,16 +1,12 @@
 const config = require('../config.json')
 const oid = process.env.oid
-const status = ["PLAYING", "WATCHING", "LISTENING"]
 
 module.exports.run = (bot, message, args, discord) => {
  
  if (message.author.id == oid) {
-  if (args[1].content == status) {
-     
-  }
  if (!args) return message.channel.send(`:ok_hand: Okay, I will set my activity back to normal!`)
- var game = args[0]
- bot.user.setActivity(`${game}`, {type: args[1]})
+ var game = args[>3]
+ bot.user.setActivity(`${game}`, {type: args[4]})
  message.channel.send(`:ok_hand: Okay, I will set my playing status to ${game}!`)
 } else {
   message.channel.send("Nope!")
