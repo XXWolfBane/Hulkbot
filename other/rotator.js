32129817375ba6b7d4771fea272afe393989f09a
FHGDev <gideon.lingle14@gmail.com>
2018-01-20 10:33:31 -0600

    Update and rename other/rotator.js to util/rotator.js

--- other/rotator.js
+++
@@ -1,3 +0,0 @@
-module.exports = (bot) => {
-    let status = ["You wouldn't like me when I'm angry.", `bot.guilds.array().length`]
-}
---
+++ util/rotator.js
@@ -0,0 +1,29 @@
+const r = "true"
+
+module.exports = (bot) => {
+    bot.on("message", message => {
+        if (!message.author.id == process.env.oid) {
+            if (message.content == "h!toggle off") {
+                r = "false"
+            }
+            if (message.content == "h!toggle on") {
+                r = "true"
+            }
+        } else {
+            message.channel.send(`Nope!`)
+        }
+    })
+    if (r == "true") {
+        let status = ["You wouldn't like me when I'm angry.", `bot.guilds.array().length`, "https://bot.hulkbot.ml/home"]
+        setInterval(() => {
+            gameval = 0;
+        if (gameval == status.length) {
+            gameval = 0;
+            }
+        var game = status[gameval]
+        bot.user.setActivity(`for h!help | ${game}`, {type: "WATCHING"})
+        }, 45000)
+    } else {
+        return;
+    }
+}
