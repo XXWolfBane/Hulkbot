6ad19e1f438ef900438aad481320935b27ca477f
FHGDev <gideon.lingle14@gmail.com>
2018-01-20 10:53:47 -0600

    Update rotator.js

--- util/rotator.js
+++ util/rotator.js
@@ -3,16 +3,16 @@ var r = "true"
 module.exports = (bot) => {
     bot.on("message", message => {
         if (message.author.id == process.env.oid) {
-             if (message.content == "h!toggle off") {
+            if (message.content == "h!toggle off") {
                 r = "false"
                 message.channel.send(`Okay, I turned my status rotator off.`)
             }
             if (message.content == "h!toggle on") {
                 r = "true"
-                message.channel.send(`Okay, I turned my status rotator back on!`)
+                message.channel.send(`Okay, I turned my status rotator back on.`)
             }
         } else {
-            message.channel.send("Nope!")
+            message.channel.send(`Nope!`)
         }
     })
     if (r == "true") {
