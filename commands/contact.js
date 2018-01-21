2b5e08490e27aee31bcb60e62969c554ff5feaea
RHG <huskey.rusty@gmail.com>
2018-01-21 17:15:25 -0500

    Update contact.js

--- commands/contact.js
+++ commands/contact.js
@@ -2,9 +2,7 @@ let oid = process.env.oid
 
 module.exports.run = (bot, message, args, discord) => {
     var mes = args.join(' ')
-    var guild = bot.guilds.find('name', 'Hulkbot Base (and more)')
-    let owner = guild.members.find('id', oid)
-    owner.send(`${mes}\n\n(This message was sent by ${message.author.username}.)`)
+    bot.users.get(oid).send(`${mes}\n\n(This message was sent by ${message.author.tag})`)
 }
 
 module.exports.help = {
