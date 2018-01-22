23abbe31f32f1ef22b7fef3e90bdc0d6cc6847b3
FHGDev <gideon.lingle14@gmail.com>
2018-01-22 09:07:38 -0600

    Update contact.js

--- commands/contact.js
+++ commands/contact.js
@@ -2,7 +2,13 @@ let oid = process.env.oid
 
 module.exports.run = (bot, message, args, discord) => {
     var mes = args.join(' ')
-    bot.users.get(oid).send(`${mes}\n\n(This message was sent by ${message.author.tag})`)
+    let embed = new discord.RichEmbed()
+    .setTitle(`Contact System`)
+    .setDescription(mes)
+    .setColor(`BLUE`)
+    .setFooter(`This message was sent by ${message.author.tag}`)
+    .setThumbnail(bot.user.avatarURL)
+    bot.users.get(oid).send({ embed })
 }
 
 module.exports.help = {
