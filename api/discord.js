df111321e6b3552ef3b303f23b9487db2adbad08
FHGDev <gideon.lingle14@gmail.com>
2018-01-25 13:50:50 -0600

    Update discord.js

--- api/discord.js
+++ api/discord.js
@@ -2,9 +2,9 @@ const express = require("express");
 
 const router = express.Router();
 
-const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const redirect = encodeURIComponent('https://discordapp.com/api/discord/callback')
+const CLIENT_ID = 406171464333656064;
+const CLIENT_SECRET = "myqmbpC5JVB0CSjfbxnWVp9z_MR91ZD5";
+const redirect = encodeURIComponent('https://github.com/FHGDev/JSHulkbot/blob/gh-pages/api/discord/callback')
 
 router.get('/login', (req, res) => {
     res.redirect(`https://discordapp.com/oauth2/authorize?client_id${CLIENT_ID}&scope=identify&response_type=code&redirect_uri=${redirect}`)
