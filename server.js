797a24f01e9690f36c05bf930bbe51d910bce1c0
FHGDev <gideon.lingle14@gmail.com>
2018-01-25 13:27:11 -0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const path = require("path");
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, 'home.html'));
+});
+
+app.listen(50451, () => {
+    console.info("Running on Port 50451.")
+});
