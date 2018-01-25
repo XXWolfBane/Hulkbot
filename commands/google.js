3893c53aa04bc7b54bfd4b675f0806b94385f4ad
FHGDev <gideon.lingle14@gmail.com>
2018-01-25 10:52:18 -0600

    Create google.js

---
+++ commands/google.js
@@ -0,0 +1,30 @@
+module.exports.run = (bot, message, args, discord) => {
+  var google = require('google')
+
+google.resultsPerPage = 25
+var nextCounter = 0
+
+google(args[0], function (err, res) => {
+  if (err) console.error(err)
+
+  for (var i = 0; i < res.links.length; ++i) {
+    var link = res.links[i];
+    let e = new discord.RichEmbed()
+    .setTitle("Hulkbot Google")
+    .setDescription(link.title + "-" + link.href)
+    .addField(link.description)
+    .setThumbnail(bot.avatarURL)
+    .setColor('RANDOM')
+    message.channel.send({ e })
+  }
+
+  if (nextCounter < 4) {
+    nextCounter += 1
+    if (res.next) res.next()
+  }
+})
+}
+
+module.exports.help = {
+  name: "search"
+}
