018b568c9f7d5603d46e3f8be463f6ba67caadf9
FHGDev <gideon.lingle14@gmail.com>
2018-01-25 21:59:26 -0600

    Update (attempt) database.js

--- (attempt) database.js
+++ (attempt) database.js
@@ -1,5 +1,21 @@
-const storage = require('node-persist')
-
-storage.init().then(() => {
-  storage.setItem('filter', 'false')
-})
+module.exports = (guild, bot, discord) => {
+  const np = require('node-persist')
+  
+  const guildsettings = new discord.Collection()
+  
+  bot.on('guildCreate', () => {
+    guildsettings.set(con)
+  })
+  
+  np.init().then(() => {
+    const con = [
+    np.setItem('guild name', guild.name),
+    np.setItem('filter', false),
+    np.setItem('prefix', "h!"),
+    np.setItem('welcome', 'Hello! Welcome to the server!'),
+    np.setItem('log', guild.channels.find('name', 'log')),
+    np.setItem('modrole', guild.roles.find('name', 'Admin')),
+    np.setItem('welcomechannel', guild.channels.find('name', "welcome")),
+      ]
+  });
+};
