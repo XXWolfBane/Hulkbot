a33e1bd612e0c07b078d345fc7f238f684d49966
FHGDev <gideon.lingle14@gmail.com>
2018-01-29 13:49:10 -0600

    Update viewstats.js

--- commands/viewstats.js
+++ commands/viewstats.js
@@ -3,7 +3,7 @@
 module.exports.run = (bot, message, args) => {
   require('request')(`https://discordbots.org/api/bots/${bot.id}/stats`, function(err, response, body) {
     if (!err && response.statusCode == 200) {
-        message.channel.send(`DiscordBots.org Stats for Hulkbot: Server Count is: ${JSON.parse(body.server_count)})
+        message.channel.send(`DiscordBots.org Stats for Hulkbot: Server Count is: ${JSON.parse(body.server_count)}`)
     } else if (err) {
         console.error(err)
     }
