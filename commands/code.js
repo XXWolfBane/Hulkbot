ada7ce0ce3ad074bf642a65378312459b668366d
FHGDev <gideon.lingle14@gmail.com>
2018-01-29 11:39:00 -0600

    Update code.js

--- commands/code.js
+++ commands/code.js
@@ -5,11 +5,11 @@ module.exports.run = (bot, message, args, discord) => {
     let em = new discord.RichEmbed()
     .setTitle(`${bot.user.displayName} Code Redemption`)
     .setDescription(`Correct Code! Use this invite to go to the Hulkbot Base discord where you'll get the SuperPatron role, and more!`)
-    .setField(`[Invite](https://discord.gg/qEFNkxB "Click to go to the Hulkbot Base!")`)
+    .addField(`[Invite](https://discord.gg/qEFNkxB "Click to go to the Hulkbot Base!")`)
     .setAuthor(`${bot.user.avatarURL} ${bot.user.displayName}`)
     .setColor(`BLUE`)
     .setFooter(new Date())
-    message.channel.send({ embed })
+    message.channel.send({ embed }).then(m => m.delete(5500))
   } else {
     message.channel.send("Sorry, incorrect code.")
   }
