c1c324f0899f3976184a6f214d174b165a16b123
FHGDev <gideon.lingle14@gmail.com>
2018-01-29 12:18:31 -0600

    Update leave.js

--- commands/leave.js
+++ commands/leave.js
@@ -1,10 +1,10 @@
 module.exports.run = (bot, message, args, discord) => {
-  if (message.author.id == message.member.guild.ownerid) {
+  if (message.author.id == message.member.guild.ownerID) {
     message.channel.send(`Fine, I'll leave... :sob:`)
     message.member.guild.leave()
   } else {
     message.channel.send(`Missing Permissions!`)
-    const err = new RangeError([`Error: User has Missing Permissions...`)])
+    const err = new RangeError([(`Error: User has Missing Permissions...`)])
     console.log(err)
   }
 }
