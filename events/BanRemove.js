6d3634fe3a977ee889b47c439e0d1d004ae0900b
FHGDev <gideon.lingle14@gmail.com>
2018-01-28 20:38:15 -0600

    Update BanRemove.js

--- events/BanRemove.js
+++ events/BanRemove.js
@@ -1,16 +1,26 @@
 module.exports = (bot, guild, member) => {
   member.send(`${member.username}, you have now been unbanned from ${guild.name}!`)
-  let hbl = ["guild-maintenance"]
- 
-  if (guild.channels.find('name' hbl)) {
-    let log = guild.channels.find('name' hbl)
-    log.send(`${member.username} has been unbanned from ${guild.name}`)
-  }
- let log = ["logs", "audit", "log", "server-maintenance"]
-  if (guild.channels.find('name', log)) {
-    let cnl = guild.channels.find('name', log)
-  cnl.send(`${member.username} has been unbanned from ${guild.name}`)
-  } else {
-    return;
-  }
+  
+  const log1="log",
+  log2="hulkbot-logs",
+  log3="audit",
+  log4="bot-audit",
+  log5="guild-maintenance"
+  
+  if (guild.channels.exists(log1)) {
+  guild.channels.find('name', log1).then(l => {
+    l.send(`${member.displayName} has been unbanned from ${guild.name}`)
+  })} else {
+  guild.channels.find('name', log2).then(l => {
+    l.send(`${member.displayName} has been unbanned from ${guild.name}`)
+  })} else {
+  guild.channels.find('name', log3).then(l => {
+    l.send(`${member.displayName} has been unbanned from ${guild.name}`)
+  })} else {
+  guild.channels.find('name', log4).then(l => {
+    l.send(`${member.displayName} has been unbanned from ${guild.name}`)
+  })} else {
+  guild.channels.find('name', log5).then(l => {
+    l.send(`${member.displayName} has been unbanned from ${guild.name}`)
+  })}
 }
