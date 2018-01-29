14833eb3cd11e017b052d83c5714d26f8a503651
FHGDev <gideon.lingle14@gmail.com>
2018-01-29 13:42:12 -0600

    Create viewstats.js

---
+++ commands/viewstats.js
@@ -0,0 +1,15 @@
+
+
+module.exports.run = (bot, message, args) => {
+  require('request')(`https://discordbots.org/api/bots/${bot.id}/stats`, function(err, response, body) {
+    if (!err && response.statusCode == 200) {
+        message.channel.send(`DiscordBots.org Stats for Hulkbot: Server Count is: ${JSON.parse(body.server_count)})
+    } else if (err) {
+        console.error(err)
+    }
+  })
+}
+
+module.exports.run = () => {
+    name: "viewstats"
+}
