0f6366bb9fe1cf5d0746a98e685193efafbef361
FHGDev <gideon.lingle14@gmail.com>
2018-01-30 11:22:51 -0600

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,9 +1,14 @@
 const oid = process.env.oid
 const prefix = require('../config.json').prefix
 
-module.exports.run = (bot, message, args) => {
-      message.channel.send("Testing...").then(msg => msg.edit('https://cdn.discordapp.com/attachments/358745922517336064/407813894078332928/giphy.gif'))
-      
+module.exports.run = (bot, message, args, discord) => {
+      let embed = new discord.RichEmbed()
+      .setTitle(`Testing`)
+      .setThumbnail(bot.user.avatarURL)
+      .setDescription(`Test Successfull!`)
+      .setImage(`https://cdn.discordapp.com/attachments/358745922517336064/407813894078332928/giphy.gif`)
+      .setAuthor(bot.user.username)
+      message.channel.send("Testing...").then(msg => msg.edit({ embed }))
     }
 
 module.exports.help = {
