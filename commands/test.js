1de7df81ea43df4b04b20a1792ce7e2ec1497118
FHGDev <gideon.lingle14@gmail.com>
2018-01-30 11:15:17 -0600

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -2,16 +2,9 @@ const oid = process.env.oid
 const prefix = require('../config.json').prefix
 
 module.exports.run = (bot, message, args) => {
-  let auth = message.author.id
-  if (auth == oid) {
-    if (message.content == prefix + "test") {
-      message.channel.send("Test Successful!")
+      message.channel.send("Testing...").then(msg => msg.edit('https://cdn.discordapp.com/attachments/358745922517336064/407813894078332928/giphy.gif'))
+      
     }
-  }
-  else {
-    message.channel.send("Command is experimental. Owner only.")
-  }
-}
 
 module.exports.help = {
   name: "test",
