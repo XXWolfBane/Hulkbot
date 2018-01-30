972c3408c2ffe9cfa530a684a3640edc40c7c060
FHGDev <gideon.lingle14@gmail.com>
2018-01-30 11:24:52 -0600

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -8,6 +8,7 @@ module.exports.run = (bot, message, args, discord) => {
       .setDescription(`Test Successfull!`)
       .setImage(`https://cdn.discordapp.com/attachments/358745922517336064/407813894078332928/giphy.gif`)
       .setAuthor(bot.user.username)
+      .setColor(`RANDOM`)
       message.channel.send("Testing...").then(msg => msg.edit({ embed }))
     }
 
