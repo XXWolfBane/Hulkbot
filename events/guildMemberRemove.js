ed6960f4b43f97494d928d26b3ab1bddb7a1ad62
FHGDev <gideon.lingle14@gmail.com>
2018-01-31 16:56:38 -0600

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -4,9 +4,11 @@ module.exports = (bot, member) => {
     let channel = member.guild.channels.find('name', 'welcome')
     if (!channel) return;
     
-    member.guild.fetchBans().then(r => if (r.find('id', member.id) {
+    member.guild.fetchBans().then(r => {
+        if (r.find('id', member.id)) {
             console.log(`${member.tag} is banned from ${member.guild.name}`)
+          }
         } else {
-            channel.send(`:dizzy_face: ${member.displayName} just left ${member.guild.name}.`)
+           channel.send(`:dizzy_face: ${member.displayName} just left ${member.guild.name}.`)
         })
 }
