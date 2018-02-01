49062cf9c7f70e138f304eb1f60158a7fca1d10d
FHGDev <gideon.lingle14@gmail.com>
2018-01-31 19:13:18 -0600

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -3,9 +3,6 @@ module.exports = (bot, member) => {
    member.send(`Welcome to ${member.guild.name}, ${member.displayName}!`)
    const channel = member.guild.channels.find('name', 'welcome')
    if (!channel) return;
-   let c = require('././(attempt) database.js')
-   let r = c.guildSettings.con
-   let wm = c.np.getItem("welcome")
    
-   channel.send(wm)
+   channel.send(`Welcome to ${member.guild.name}, ${member.displayName}!`)
 }
