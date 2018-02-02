1b57b22f346abd8a0ba822b54bb89f5e0958e4eb
FHGDev <gideon.lingle14@gmail.com>
2018-02-01 22:53:39 -0600

    Update cleverbot.js

--- events/cleverbot.js
+++ events/cleverbot.js
@@ -3,11 +3,11 @@ var cleverbot = require('cleverbot.io'),
 cb = new cleverbot("sMNApmkOjMlZRlPZ", "gskxw3JBqEVGIAboBjOnvyTf8awM1MbS")
 
 module.exports = (bot, message) => {
+  if (message.author.bot) return;
   if (message.content.startsWith(prefix)) return;
   cb.setNick("Hulkbot")
   if (!message.content.toLowerCase().includes("who is your creator")) {
-    if (message.author.bot) return;
-      cb.create(function (err, session) {
+        cb.create(function (err, session) {
         cb.ask(message.content, function (err, response) {
         message.channel.send(response)
         console.log(`${message.author.username}(${message.author.id}) said ${message.content}, and Hulkbot responded with ${response}!`)
