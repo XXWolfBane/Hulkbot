6188456d85d1ce0686f0fa9ac983a17e7b60a003
FHGDev <gideon.lingle14@gmail.com>
2018-02-02 14:31:15 -0600

    Update viewstats.js

--- commands/viewstats.js
+++ commands/viewstats.js
@@ -1,14 +1,28 @@
-module.exports.run = (bot, message, args) => {
-  require('request')(`https://discordbots.org/api/bots/${bot.id}/stats`, function(err, response, body) {
-    if (!err && response.statusCode == 200) {
-        message.channel.send(`DiscordBots.org Stats for Hulkbot: Server Count is: ${JSON.parse(body.server_count)}`)
-    } else if (err) {
-        console.error(err)
-        message.channel.send(`Rip I can't find anything...`)
-    }
+const dbl = require('discord-bot-list')
+
+module.exports.run = (bot, message, args, discord) => {
+  const c = new dbl({
+    token: process.env.t,
+    id: bot.id
   })
+  
+  let arg = args.slice(0)
+  if (message.content == "h!dblsearch user") {
+    if (arg[0].length !== 18) return;
+      if (!arg[0] == NaN) {
+        c.getUser(arg[0], (error, res) => {
+          let em = new discord.RichEmbed()
+          .setTitle(`Discord Bot List User Search`)
+          .setDescription(`This is what I got: ${res}`)
+          .setAuthor(`Hulkbot`)
+          .setColor("RANDOM")
+          .setThumbnail(bot.user.avatarURL)
+          .setImage(`https://i.imgur.com/aId29cQ.png`)
+      })
+    } 
+  } 
 }
 
 module.exports.help = {
-    name: "viewstats"
+    name: "dblsearch"
 }
