a58dc925cc58d4a500c11446346ca62a8072faa8
FHGDev <gideon.lingle14@gmail.com>
2018-02-01 22:41:21 -0600

    Update cleverbot.js

--- events/cleverbot.js
+++ events/cleverbot.js
@@ -3,10 +3,10 @@ var cleverbot = require('cleverbot.io'),
 cb = new cleverbot("sMNApmkOjMlZRlPZ", "gskxw3JBqEVGIAboBjOnvyTf8awM1MbS")
 
 module.exports = (bot, message) => {
-  if (message.author.bot) return;
   if (message.content.startsWith(prefix)) return;
   cb.setNick("Hulkbot")
   if (!message.content.toLowerCase().includes("who is your creator")) {
+    if (message.author.bot) return;
       cb.create(function (err, session) {
         cb.ask(message.content, function (err, response) {
         message.channel.send(response)
