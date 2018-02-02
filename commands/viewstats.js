a87642ed1dff4040469f292e0169620c05477be6
FHGDev <gideon.lingle14@gmail.com>
2018-02-02 16:07:03 -0600

    Update viewstats.js

--- commands/viewstats.js
+++ commands/viewstats.js
@@ -9,22 +9,8 @@ module.exports.run = (bot, message, args, discord) => {
   setInterval(() => {
     c.postStats(bot.guilds.array().length, (err) => {
       if (err) console.error(new RangeError([`Failed to Post Stats to https://discordbots.org. Error Message: ${err}`]))
-    })
+    }, 100)
   })
-  let v = c.getVotes()
-  let s = c.getBot()
-  let votes = v
-  let stats = s
-  
-  let em = new discord.RichEmbed()
-  .setTitle(`Discord Bot List Stats`)
-  .setDescription(`Discord Bot List Hulkbot Stats:`)
-  .addField(`Bot Upvotes: ${votes}`)
-  .addField(`Bot Name: ${stats}`)
-  .addField(`Bot Status: ${bot.status}`)
-  .setThumbnail(bot.user.avatarURL)
-  .setAuthor(bot.user.username)
-  .setImage('https://i.imgur.com/aId29cQ.png')
 }
 
 module.exports.help = {
