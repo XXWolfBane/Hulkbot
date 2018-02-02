e0894d29edf6248ed68c53adb0dcfd505681caaf
FHGDev <gideon.lingle14@gmail.com>
2018-02-01 19:18:11 -0600

    Added "Who is your creator" support.

--- events/cleverbot.js
+++ events/cleverbot.js
@@ -6,13 +6,17 @@ module.exports = (bot, message) => {
   if (message.author.bot) return;
   if (message.content.startsWith(prefix)) return;
   cb.setNick("Hulkbot")
-  cb.create(function (err, session) {
-    cb.ask(message.content, function (err, response) {
-      message.channel.send(response)
-      console.log(`${message.author.username}(${message.author.id}) said ${message.content}, and Hulkbot responded with ${response}!`)
-      if (err) {
-        console.error
-      }
+  if (!message.content.toLowerCase().includes("who is your creator") {
+      cb.create(function (err, session) {
+        cb.ask(message.content, function (err, response) {
+        message.channel.send(response)
+        console.log(`${message.author.username}(${message.author.id}) said ${message.content}, and Hulkbot responded with ${response}!`)
+        if (err) {
+          console.error
+        }
+      })
     })
-  })
+  } else {
+    message.channel.send(`My creators are <@${process.env.oid}> and <@140487710727995392>.`)
+  }
 }
