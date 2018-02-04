33c6fb74923218b16b7025a8ac3f2824e6876b0e
FHGDev <gideon.lingle14@gmail.com>
2018-02-04 14:36:25 -0600

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -1,4 +1,5 @@
 module.exports.run = (bot, message, args) => {
+	if (message.channel.type == "dm") {
 	if (message.member.hasPermission(["MANAGE_MESSAGES"], false, true, true)) {
   if (isNaN(args[0])) {
     return message.channel.send('Please define a number..')
@@ -16,12 +17,23 @@ module.exports.run = (bot, message, args) => {
 
   setTimeout(function() {
 	message.channel.fetchMessages({limit: am}).then(m => message.channel.bulkDelete(m))
-	message.channel.send("Done! Purged " + am + " messages.")
+	message.channel.send("Done! Purged " + am + " messages!")
   }, 1000);
 } else {
 	message.channel.send("Sorry, you don't have the required permissions. :neutral_face:")
 }
-
+} else {
+	let am = args[0]
+	if (am>190) {
+		message.channel.send("The maximum amount you can delete is 90 messages.")
+		return;
+	}
+	message.channel.send(":exclamation: Beginning to purge " + am + " messages.")
+	setTimeout(() => {
+	      message.channel.fetchMessages({limit: am}).then(m => message.channel.bulkDelete(m))
+              message.channel.send("Done! Purged " + am + " messages!")
+	}, 1000)
+}
 }
 
 module.exports.help = {
