aba704e09f44d46ccebc5755fd8e6bf846e06a63
FHGDev <gideon.lingle14@gmail.com>
2018-02-04 20:00:42 -0600

    Create creators.js

---
+++ commands/creators.js
@@ -0,0 +1,7 @@
+module.exports.run = (bot, message, args, discord) => {
+  message.channel.send(`My creators are <@${process.env.oid} and <@140487710727995392>.`)
+}
+
+module.exports.help = {
+  name: "creators"
+}
