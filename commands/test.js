cc4c6f1e3e1555a344e1e47f14df21a4162272db
FHGDev <gideon.lingle14@gmail.com>
2018-02-05 12:58:00 -0600

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,13 +1,10 @@
-const oid = process.env.oid
-const prefix = require('../config.json').prefix
-
 module.exports.run = (bot, message, args, discord) => {
       let embed = new discord.RichEmbed()
       .setTitle(`Testing`)
       .setThumbnail(bot.user.avatarURL)
       .setDescription(`Test Successfull!`)
       .setImage(`https://cdn.discordapp.com/attachments/358745922517336064/407813894078332928/giphy.gif`)
-      .setAuthor(`${bot.user.avatarURL} ${bot.user.username}`)
+      .setAuthor(bot.user.username)
       .setColor(`RANDOM`)
       message.channel.send("Testing...").then(msg => msg.edit({ embed }))
     }
