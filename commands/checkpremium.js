9b77f57ce5d80c185b01b4522635a3ae673bf133
FHGDev <gideon.lingle14@gmail.com>
2018-02-05 20:03:39 -0600

    Create checkpremium.js

---
+++ commands/checkpremium.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+const config = require('././config.json')
+module.exports.run = (bot, message, args, discord) => {
+    let ga = bot.guilds.get('356178662837452800')
+    let member = message.author
+    let json = JSON.stringify("premium": "true")
+    if (ga.members.find('id', member.id).hasRole('SuperPatron')) {
+        fs.writeFile(config, json)
+        if (config.premium == "true") {
+            let em = new discord.RichEmbed()
+            .setTitle("Hulkbot Premium")
+            .setDescription(`Yes! Your premium status is active!`)
+            .setAuthor(bot.user.username)
+            .setThumbnail(bot.user.avatarURL)
+            message.channel.send({ em })
+        }
+    } else {
+    fs.writeFile(config, "premium": "false")
+        let em = new discord.RichEmbed()
+        .setTitle("Hulkbot Premium")
+        .setDescription(`No. Your premium status is unactive. Buy premium [here!](https://www.patreon.com/bePatron?c=1365207 "buy premium")`)
+        .setAuthor(bot.user.username)
+        .setThumbnail(bot.user.avatarURL)
+        message.channel.send({ em })
+    }
+}
