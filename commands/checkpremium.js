89567f40c5475602e9b2b8ab5887b9bae6f8e8a4
FHGDev <gideon.lingle14@gmail.com>
2018-02-05 20:12:43 -0600

    Update checkpremium.js

--- commands/checkpremium.js
+++ commands/checkpremium.js
@@ -3,7 +3,7 @@ const config = require('././config.json')
 module.exports.run = (bot, message, args, discord) => {
     let ga = bot.guilds.get('356178662837452800')
     let member = message.author
-    let json = JSON.stringify("premium": "true")
+    let json = JSON.stringify(`"premium": "true"`)
     if (ga.members.find('id', member.id).hasRole('SuperPatron')) {
         fs.writeFile(config, json)
         if (config.premium == "true") {
