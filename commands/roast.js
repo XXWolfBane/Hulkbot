87cb01bc3e98aef868047e808e8a632556f00af8
FHGDev <gideon.lingle14@gmail.com>
2018-02-08 14:49:33 -0600

    Create roast.js

---
+++ commands/roast.js
@@ -0,0 +1,18 @@
+module.exports.run = (bot, message, args, discord) => {
+  let num = Math.floor(Math.random()*1000)
+  let num2 = Math.floor(Math.random()*num)
+  let member = message.mentions.members.first();
+  
+  let em = new discord.RichEmbed()
+  .setTitle("Hulkbot Roasting")
+  .setDescription(`${member.displayName} has a ${num2}% chance of failing in life.`)
+  .setFooter(`${member.displayName} got roasted by ${message.author.displayName}!`)
+  .setThumbnail(`https://i.imgur.com/6Bzv6Wv.png`)
+  .setAuthor(`Hulkbot`)
+  
+  message.channel.send({ em })
+}
+
+module.exports.help = {
+  name: "roast"
+}
