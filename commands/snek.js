3235c41303af4513a6cd8169959cd59b26c9d820
FHGDev <gideon.lingle14@gmail.com>
2018-02-10 13:37:54 -0600

    Create snek.js

---
+++ commands/snek.js
@@ -0,0 +1,12 @@
+ module.exports.run = (bot, message, args, discord) => {
+  let em = new discord.RichEmbed()
+  .setTitle(`Hulkbot Snek`)
+  .setDescription(`I am big snek. Are you?`)
+  .setImage("https://media.giphy.com/media/26DMWpQjDSf4AI9Ik/giphy.gif")
+  .setThumbnail(bot.user.avatarURL)
+  .setColor("RANDOM")
+ }
+ 
+ module.exports.help = {
+  name: "snek"
+ }
