edb7ee1665f6cad2479d5f3075def9a66373aaf2
FHGDev <gideon.lingle14@gmail.com>
2018-02-19 14:57:08 -0600

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -1,7 +1,10 @@
 module.exports.run = (bot, message, args, discord) => {
   const m = require('automeme')
   m.getMeme((err, meme) => {
-    if (err) return console.error(err)
+    if (err) {
+      message.channel.send("I couldn't get a meme...")
+      return console.error(err)
+    }
     let em = new discord.RichEmbed()
     .setTitle("Hulkbot Meme Generator")
     .setDescription("Here's a random meme!")
