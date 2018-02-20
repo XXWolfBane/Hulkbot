c6a6efbe48b7928d9135472cf52007bd59176cbf
FHGDev <gideon.lingle14@gmail.com>
2018-02-20 13:51:23 -0600

    Update checkpremium.js

--- commands/checkpremium.js
+++ commands/checkpremium.js
@@ -3,10 +3,12 @@ const config = require('../config.json')
 module.exports.run = (bot, message, args, discord) => {
     let ga = bot.guilds.get('356178662837452800')
     let member = message.author
-    let json = JSON.stringify(`"premium": "true"`)
     if (ga.members.find('id', member.id).roles.find('name','SuperPatron')) {
-        fs.writeFile(config, json)
-        if (config.premium == "true") {
+        let json = "true"
+        config.premiumState = "true"
+        fs.writeFile('../config.json', JSON.stringify(json), (err) => console.error(err))
+        
+        if (config.premiumState == "true") {
             let em = new discord.RichEmbed()
             .setTitle("Hulkbot Premium")
             .setDescription(`Yes! Your premium status is active!`)
@@ -15,13 +17,16 @@ module.exports.run = (bot, message, args, discord) => {
             message.channel.send({ em })
         }
     } else {
-    let json = (`"premium": "false"`)
-    fs.writeFile(config, json)
+        let premiumState = "false"
+        config.premiumState = "false";
+        let json = "true"
+    fs.writeFile('../config.json', JSON.stringify(json), (err) => console.error(err))
         let em = new discord.RichEmbed()
         .setTitle("Hulkbot Premium")
         .setDescription(`No. Your premium status is unactive. Buy premium [here!](https://www.patreon.com/bePatron?c=1365207 "buy premium")`)
         .setAuthor(bot.user.username)
         .setThumbnail(bot.user.avatarURL)
+        .setColor("RANDOM")
         message.channel.send({ em })
     }
 }
