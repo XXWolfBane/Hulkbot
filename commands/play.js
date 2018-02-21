0acb2fe9474fbe1f9a3db03ac28bc64df5fa96f7
FHGDev <gideon.lingle14@gmail.com>
2018-02-21 14:04:49 -0600

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -2,20 +2,6 @@ const YTDL = require('ytdl-core')
 var servers = {}
 
 module.exports.run = (bot, message, args, discord) => {
-   function play(connection, message) {
-   message.channel.send("I have started playing " + args[0] + " in " + message.member.voiceChannel.name + ".")
-   var server = servers[message.guild.id]
-   
-   server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}))
-   
-   server.queue.shift()
-   
-   server.dispatcher.on('end', () => {
-      if (server.queue[0]) play(connection, message)
-      else connection.disconnect()
-    })
-  }
-   
   if (!args[0]) {
     message.channel.send("Please provide a song name/link!");
     return;
@@ -28,6 +14,19 @@ module.exports.run = (bot, message, args, discord) => {
     queue: []
   }
   if (!message.member.voiceChannel) message.member.voiceChannel.join().then((connection) => {
+     function play(connection, message) {
+   message.channel.send("I have started playing " + args[0] + " in " + message.member.voiceChannel.name + ".")
+   var server = servers[message.guild.id]
+   
+   server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}))
+   
+   server.queue.shift()
+   
+   server.dispatcher.on('end', () => {
+      if (server.queue[0]) play(connection, message)
+      else connection.disconnect()
+    })
+  }
     play(connection, play);
   })
    
