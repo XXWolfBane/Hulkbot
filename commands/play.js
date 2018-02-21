aecfa653f0c226930e5ab211ecf8d6655058d552
FHGDev <gideon.lingle14@gmail.com>
2018-02-21 13:52:16 -0600

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -4,7 +4,7 @@ var servers = {}
 function play(connection, message) {
    var server = servers[message.guild.id]
    
-   server.dispatcher = connection.playStream(YTDL(server.queue[0], filter: "audioonly"}, ))
+   server.dispatcher = connection.playStream(YTDL(server.queue[0], filter: "audioonly"}))
    
    server.queue.shift()
    
