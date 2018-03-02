c9d508a10e1f1ab7376a4cf2efe6061f47970723
FHGDev <gideon.lingle14@gmail.com>
2018-03-02 17:23:05 -0600

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,5 +1,13 @@
 const fs = require('fs')
 
+const date = [new Date().getHours(), new Date().getMinutes()],
+options = {
+  hour: 'numeric',
+  minute: 'numeric',
+  hour12: true
+}
+const da = date.toLocaleString('en-us', options)
+
 const db = require('../database.json')
 
 module.exports = (bot, guild, discord) => {
@@ -15,9 +23,14 @@ module.exports = (bot, guild, discord) => {
   .setTitle("Introduction to Hulkbot")
   .setAuthor(bot.user.username)
   .setThumbnail(bot.user.avatarURL)
-  .setFooter(`This message was sent today at ${new Date().getHours()}:${new Date().getMinutes()}`)
+  .setFooter(`This message was sent today at ${da}`)
   
   bot.users.get(guild.ownerID).send({embed})
  
-  fs.writeFile('../database.json', json, (err) => console.error(err))
+  fs.writeFile('../database.json', json, (err) => {
+    if (err) {
+      console.error(err)
+    } else 
+      console.log('Added to database.')
+  })
 }
