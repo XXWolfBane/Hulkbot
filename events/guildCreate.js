2b0618835e147f55ae296e53c1e930093f589802
FHGDev <gideon.lingle14@gmail.com>
2018-03-02 15:10:00 -0600

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -5,9 +5,15 @@ const db = require('../database.json')
 module.exports = (bot, guild, discord) => {
   db.guildname = guild.name
   
-  fs.writeFile('../database.json', JSON.stringify(guild.name), (err) => console.error(err))
+  let embed = new discord.RichEmbed()
+  
+  bot.users.get(guild.ownerID).send({embed})
+  
+  fs.writeFile('../database.json', JSON.stringify(guild.name), 'utf8', (err) => console.error(err))
   
   const membercount = guild.memberCount
+  
+  db.membercount = membercount
   
   fs.writeFile('../database.json', JSON.stringify(membercount), (err) => console.error(err))
 }
