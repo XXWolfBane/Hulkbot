741c70d345d1d8c64429ce8fa50fd933ec5549f4
FHGDev <gideon.lingle14@gmail.com>
2018-03-04 19:46:27 -0600

    Update filter.js

--- util/filter.js
+++ util/filter.js
@@ -1,7 +1,8 @@
 module.exports = (bot, message, discord) => {
-  if (message.channel.name == "bot-cmds") {
+  if (message.channel.name == "bot-cmds")) {
     if (message.content.startsWith("h!") {
-    message.delete("No bot commands here, go to #bot-cmds. Thanks! :D").then(m => m.delete(2000)
+        message.delete(2500)
+        message.channel.send("No bot commands here, go to #bot-cmds. Thanks! :D").then(m => m.delete(2000)
   }
  }
 }
