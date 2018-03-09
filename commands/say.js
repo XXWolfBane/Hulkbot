d4bbc22e12381062c1a540cc78b42a23ad943979
FHGDev <gideon.lingle14@gmail.com>
2018-03-08 19:58:04 -0600

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,4 +1,7 @@
 module.exports.run = (bot, message, args) => {
+ if (message.guild.id == '318532861638737931') {
+  if (!message.member.hasPermission("ADMINISTRATOR")) return;
+ }
  message.delete(1000)
  message.channel.send(args.join(' '))
 }
