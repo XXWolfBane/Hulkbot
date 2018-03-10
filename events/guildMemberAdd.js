d1e039ddb30ef5254aace1de71c66180f4a961ff
FHGDev <gideon.lingle14@gmail.com>
2018-03-10 15:11:16 -0600

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,4 +1,9 @@
 module.exports = (bot, member) => {
+   let guildid = 318532861638737931
+   
+   if (member.guild.id == guildid) {
+      return;
+   }
    console.log(`${member.displayName} just joined ${member.guild.name}`) 
    //member.send(`Welcome to ${member.guild.name}, ${member.displayName}!`)
    const channel = member.guild.channels.find('name', 'welcome')
