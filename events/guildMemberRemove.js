30bcbbfbbc7f5497b600ae44690cd213da8e5595
FHGDev <gideon.lingle14@gmail.com>
2018-03-10 15:12:39 -0600

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,6 +1,10 @@
 module.exports = (bot, member) => {
     console.log(`${member.displayName} just left ${member.guild.name}.`)
+    let guildid = 318532861638737931
     
+    if (member.guild.id == guildid) {
+        return;
+    }
     let channel = member.guild.channels.find('name', 'welcome')
     if (!channel) return;
     
