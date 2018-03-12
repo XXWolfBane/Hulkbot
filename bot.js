7e1b079288e7467642e3b6b5cb9ae54a8491c4e5
FHGDev <gideon.lingle14@gmail.com>
2018-03-12 00:18:18 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -44,10 +44,10 @@ bot.on("ready", () => {
   });
 });
 
-bot.on("guildMemberAdd", (member) => require('./events/guildMemberAdd.js')(bot, member))
-bot.on("guildMemberRemove", (member) => require('./events/guildMemberRemove.js')(bot, member))
-bot.on("guildBanAdd", (guild, member) => require('./events/BanAdd.js')(bot, guild, member))
-bot.on("guildBanRemove", (guild, member) => require('./events/BanRemove.js')(bot, guild, member))
+//bot.on("guildMemberAdd", (member) => require('./events/guildMemberAdd.js')(bot, member))
+//bot.on("guildMemberRemove", (member) => require('./events/guildMemberRemove.js')(bot, member))
+//bot.on("guildBanAdd", (guild, member) => require('./events/BanAdd.js')(bot, guild, member))
+//bot.on("guildBanRemove", (guild, member) => require('./events/BanRemove.js')(bot, guild, member))
  
 bot.on("message", message => {
   if (filteron == "true") {
