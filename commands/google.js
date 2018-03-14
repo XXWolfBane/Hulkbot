113836b54cab735e39ba71236833249452b934b5
FHGDev <gideon.lingle14@gmail.com>
2018-03-13 20:32:14 -0500

    Update google.js

--- commands/google.js
+++ commands/google.js
@@ -1,31 +1,11 @@
 module.exports.run = (bot, message, args, discord) => {
-  var google = require('google')
-
-google.resultsPerPage = 25
-var nextCounter = 0
-
-google(args[0], function (err, res) {
-  if (err) console.error(err)
-
-  for (var i = 0; i < res.links.length; ++i) {
-    var link = res.links[i];
-    let e = new discord.RichEmbed()
-    .setTitle("Hulkbot Google")
-    .setDescription(link.title + "-" + link.href)
-    .setThumbnail(bot.avatarURL)
-    .setColor('RANDOM')
-    bot.user.startTyping()
-    setTimeout(() => {
-      message.channel.send({ e })
-      bot.user.stopTyping()
-    }, 1000)
-  }
-
-  if (nextCounter < 4) {
-    nextCounter += 1
-    if (res.next) res.next()
-  }
-})
+ const chan = message.channel
+ const googleIt = require('google-it');
+ let search = args.join(' ');
+  
+ googleIt({'query', search}).then(results => {
+    chan.send(`These are the results I found for ${search}:\n${results}`)
+  }).catch(error => console.error(err))
 }
 
 module.exports.help = {
