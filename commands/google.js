8bddaa7e641b24b275d229fee4c45193e57f7da6
FHGDev <gideon.lingle14@gmail.com>
2018-03-13 20:37:57 -0500

    Update google.js

--- commands/google.js
+++ commands/google.js
@@ -3,7 +3,7 @@ module.exports.run = (bot, message, args, discord) => {
  const googleIt = require('google-it');
  let search = args.join(' ');
   
- googleIt({'query', search}).then(results => {
+ googleIt({'query': search}).then(results => {
     chan.send(`These are the results I found for ${search}:\n${results}`)
   }).catch(error => console.error(err))
 }
