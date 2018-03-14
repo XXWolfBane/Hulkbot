ae99959f3cf479b36624a735eed492135bffffea
FHGDev <gideon.lingle14@gmail.com>
2018-03-13 20:42:20 -0500

    Update google.js

--- commands/google.js
+++ commands/google.js
@@ -1,13 +1,31 @@
-module.exports.run = (bot, message, args, discord) => {
- const chan = message.channel
- const googleIt = require('google-it');
- let search = args.join(' ');
-  
- googleIt({'query': search}).then(results => {
-    chan.send(`These are the results I found for ${search}:\n${results}`)
-  }).catch(error => console.error(err))
+const cheerio = require('cheerio');
+const snekfetch = require('snekfetch');
+const querystring = require('querystring');
+
+module.exports.run = (bot, message, args) => {
+  if (!args[0]) {
+    message.channel.send(`:x: You need a item to search google.`);
+    return;
+  }
+
+  let searchUrl = `https://www.google.com/search?q=${encodeURIComponent(message.content)}`;
+  message.channel.send(`Searching google for ${args.join(' ')}...`).then(m => m.delete(2500))
+  return snekfetch.get(searchUrl).then((result) => {
+
+    // Cheerio lets us parse the HTML on our google result to grab the URL.
+    let $ = cheerio.load(result.text);
+    let googleData = $('.r').first().find('a').first().attr('href');
+
+    googleData = querystring.parse(googleData.replace('/url?', ''));
+    message.channel.send(`Results found on google for ${args.join(' ')}:\n${googleData.q}`).then(m => m.delete(5500))
+
+  }).catch((err) => {
+    message.channel.send(`No results found on google for ${args.join(' ')}...`).then(m => m.delete(2500))
+  });
 }
 
 module.exports.help = {
-  name: "search"
+  name: "google",
+  usage: `(string)`,
+  information: "Let's search google!"
 }
