6dd3438d5666554545332b7df5f2a2ea486e0e27
FHGDev <gideon.lingle14@gmail.com>
2018-03-23 19:42:31 -0500

    Update setnick.js

--- commands/setnick.js
+++ commands/setnick.js
@@ -2,7 +2,10 @@ module.exports.run = (bot, message, args, discord) => {
   let newname = args.join(' ')
   bot.user.setUsername(newname)
   
-  message.channel.send(`Okay, I set my username to ${newname}!`)
+  message.channel.send(`Okay, I set my username to ${newname}!`).then(m => {
+    message.delete(100)
+    m.delete(100)
+  })
 }
 
 module.exports.help = {
