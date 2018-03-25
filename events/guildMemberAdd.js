93940dd048f9d3b12cecdebd6a243f6515fdfb04
FHGDev <gideon.lingle14@gmail.com>
2018-03-25 18:20:26 -0500

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,13 +1,26 @@
 module.exports = (bot, member) => {
-   let guildids = [318532861638737931, 264445053596991498,  110373943822540800]
+   let guildids = ["318532861638737931", "264445053596991498",  "110373943822540800"]
    
-   if (member.guild.id == guildid) {
+   if (member.guild.id.includes(guildids)) {
       return;
    }
    console.log(`${member.displayName} just joined ${member.guild.name}`) 
-   //member.send(`Welcome to ${member.guild.name}, ${member.displayName}!`)
+   // SOON: member.send(`Welcome to ${member.guild.name}, ${member.displayName}!`)
    const channel = member.guild.channels.find('name', 'welcome')
    if (!channel) return;
-   
-   channel.send(`Welcome to ${member.guild.name}, ${member.displayName}!`)
+   bot.on("message", message => {
+      if (message.channel.type == "dm") {
+         return;
+      } else {
+      if (message.content == "h!disablewelcome") {
+         if (message.member.hasPermission("MANAGE_SERVER")) {
+                  message.channel.send("Welcome Messages were turned off.")
+         } else {
+            message.channel.send("No permissions... Sorry fam.")
+         }
+      } else {
+         channel.send(`Welcome to ${member.guild.name}, ${member.displayName}!`)
+      }
+     }
+   })
 }
