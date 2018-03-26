e16d23fb747dcde17e50686ca662d7d31380140d
FHGDev <gideon.lingle14@gmail.com>
2018-03-26 14:09:45 -0500

    Update guildDelete.js

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,3 +1,9 @@
-module.exports = (bot, guild, discord) = {
-
+module.exports = (bot, guild, discord) => {
+  let embed = new discord.RichEmbed()
+  .setTitle("Hulkbot's Final Message.")
+  .setDescription("Aw, sorry you weren't satisfied with me... Please tell my creator what's wrong by saying `h!contact msg`, or just going to https://github.com/FHGDev/JSHulkbot/issues/new. Thanks!")
+  .setAuthor(bot.user.avatarURL, bot.user.username)
+  .setTimestamp()
+  .setColor("RED")
+  bot.users.get(guild.ownerID).send({ embed })
 }
