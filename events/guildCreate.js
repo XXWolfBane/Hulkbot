e57a8df6d30ce3f7ea5d35dc14ad572902163794
FHGDev <gideon.lingle14@gmail.com>
2018-03-26 15:11:14 -0500

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -5,7 +5,7 @@ module.exports = (bot, guild, discord) => {
   let embed = new discord.RichEmbed()
   .setTitle("Introduction to Hulkbot")
   .addField("Heyo! Thanks for adding me.","Before you start using me, I need to get some things straight. First of all, for some basic info about me, say `h!info`.")
-  .addField("To see a list of commands","Say `h!help`.", "Inline" true)
+  .addField("To see a list of commands","Say `h!help`.", true)
   .addField("If you need to report bug:","go to the [issue tracker](https://github.com/FHGDev/JSHulkbot/issues/new), or just use the `h!contact` command, and it will send a message straight to my creator.")
   .addField("Done!", "Alrighty! Now you can start using all my awesome features in your server, "+member.name+"!")
   .setThumbnail(bot.user.avatarURL)
