0691380cffdde2cae5285aa3d76a56561d9cc5e1
FHGDev <gideon.lingle14@gmail.com>
2018-03-29 20:12:14 -0500

    Update unhackban.js

--- commands/unhackban.js
+++ commands/unhackban.js
@@ -1,8 +1,11 @@
 module.exports.run = (bot, message, args, discord) => {
   let id = args.join(' ');
-  let members = bot.fetchUsers()
-  if (members.includes(id)) {
-   message.guild.unban(id) 
+  let member = bot.fetchUser(id)
+  if (member) {
+    message.guild.unban(member)
+    message.channel.send(`Alright, I just unhackbanned ${member.name}, with the id ${member.id}.`)
+  } else {
+    message.channel.send(id + " is not a valid Discord UserID.")
   }
 }
 
