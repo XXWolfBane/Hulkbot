476728be8bbbe8ed2c8f937ae5d8ad49731baf7b
FHGDev <gideon.lingle14@gmail.com>
2018-04-07 10:57:30 -0500

    Update rotator.js

--- util/rotator.js
+++ util/rotator.js
@@ -1,6 +1,6 @@
 const r = "true"
 
-module.exports = (bot => {
+module.exports = (bot) => {
     bot.on("message", message => {
         if (message.author.id == process.env.oid) {
             if (message.content == "h!toggle off") {
