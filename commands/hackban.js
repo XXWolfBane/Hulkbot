2a73a2573b4be85aeb27abe2c980fa4380606e03
FHGDev <gideon.lingle14@gmail.com>
2018-04-09 16:13:49 -0500

    Update hackban.js

--- commands/hackban.js
+++ commands/hackban.js
@@ -2,10 +2,11 @@ module.exports.run = (bot, message, args, discord) => {
   let mid = args.join(' ');
   
     bot.fetchUser(mid).then(id => {
-      message.guild.ban(id).then(message.channel.send("Just hackbanned the user with ID "+id+".").catch(err => {
+      message.guild.ban(id).catch(err => {
         message.channel.send("Failed to ban user with the id "+id+".")
         console.log(err)
       })
+      message.channel.send(`Alright, I banned the user with ID ${id}.`)
     }).catch(() => {
       message.channel.send(`There's no user with the ID of ${mid}, please try again. :face_palm:`)
     })
