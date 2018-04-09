4448c4b21449e4f103baded06237b8c03eb8b193
FHGDev <gideon.lingle14@gmail.com>
2018-04-09 15:10:20 -0500

    Update hackban.js

--- commands/hackban.js
+++ commands/hackban.js
@@ -1,12 +1,13 @@
 module.exports.run = (bot, message, args, discord) => {
   let id = args.join(' ');
-  let member = bot.fetchUser(id)
-  if (bot.fetchUser(id)) {
-  message.guild.ban(member.id)
-  message.channel.send(`Alright, I just banned ${id}.`)
-} else {
-    message.channel.send("There's no user on Discord with that ID... :C")
+  if (!id == typeof("number")) {
+    message.channel.send('I need a number, not anything else.')
   }
+  bot.fetchUser(id).then(member => {
+    member.ban()
+  }).catch(() => {
+    message.channel.send(`There's no user with the ID of ${id}, please try again. :face_palm:`)
+  })
 }
 
 module.exports.help = {
