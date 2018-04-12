da958ecf0751d281980c400e89098c3e283f20fd
FHGDev <gideon.lingle14@gmail.com>
2018-04-11 19:45:04 -0500

    Update changegame.js

--- commands/changegame.js
+++ commands/changegame.js
@@ -7,9 +7,10 @@ module.exports.run = (bot, message, args, discord) => {
    if (!game) {
     message.channel.send(`:ok_hand: Okay, I will set my activity back to normal!`)
     bot.user.setActivity(`for h!help | ${bot.guilds.size} servers`, {type: "WATCHING"})
-  }
+  } else {
     bot.user.setActivity(`${game}`, {type: "PLAYING"})
     message.channel.send(`:ok_hand: Okay, I will set my activity to '${game}'!`)
+  }
   } else {
      message.channel.send("Nope!")
   } 
