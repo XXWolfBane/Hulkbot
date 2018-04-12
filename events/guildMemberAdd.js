8f0c474bdcc4c62b9c6f5fde0b09bf164e1bbe33
FHGDev <gideon.lingle14@gmail.com>
2018-04-12 14:27:57 -0500

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,12 +1,29 @@
 module.exports = (bot, member) => {
-   let guildids = ["318532861638737931", "264445053596991498",  "110373943822540800"]
+   let guildids = ["318532861638737931", "264445053596991498",  "110373943822540800"],
+   autoroles = {
+      "robloxlovers": "428953426840256513",
+      "learning": "434070145397161986"
+   },
+   autoroleguilds = {
+     "1": "426909155832365066",
+     "2": "419160188541599744"
+   }
    
    if (guildids.includes(member.guild.id)) {
       return;
-   }
-   console.log(`${member.displayName} just joined ${member.guild.name}`) 
-   // SOON: member.send(`Welcome to ${member.guild.name}, ${member.displayName}!`)
-   const channel = member.guild.channels.find('name', 'welcome')
+   } else {
+   console.log(`${member.displayName} just joined ${member.guild.name}`);
+   // SOON: member.send(`Welcome to ${member.guild.name}, ${member.displayName}!`);
+   const channel = member.guild.channels.find('name', 'welcome');
    if (!channel) return;
-         channel.send(`Welcome to ${member.guild.name}, ${member.displayName}!`)
+         channel.send(`Welcome to ${member.guild.name}, ${member.displayName}!`);
+   }
+   
+   if (member.guild.id == autoroleguilds.1) {
+      member.addRole(autoroles.robloxlovers)
+   } else {
+      if (member.guild.id == autoroleguilds.2) {
+         member.addRole(autoroles.learning)
+      }
+   }
 }
