0d954530d516eac32e118a902d431979c3aaa44a
FHGDev <gideon.lingle14@gmail.com>
2018-04-11 19:34:13 -0500

    Update baselog.js

--- commands/baselog.js
+++ commands/baselog.js
@@ -8,7 +8,7 @@ module.exports.run = (bot, message, args, discord) => {
   .setImage(bot.user.avatarURL)
   .setAuthor(bot.user.username)
   if (message.author.id == process.env.oid) {
-        message.channel.send({ em })
+        message.channel.send({embed: em})
         baselogger(bot, txt, bot.avatarURL) 
   } else {
     message.channel.send("Nope!")
