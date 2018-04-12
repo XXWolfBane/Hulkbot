7ccc236196195258ba3ffec765bd7b23b023705a
FHGDev <gideon.lingle14@gmail.com>
2018-04-11 19:29:57 -0500

    Update baselog.js

--- commands/baselog.js
+++ commands/baselog.js
@@ -1,9 +1,14 @@
 const {baselogger} = require('../logger.js')
 
-module.exports.run = (bot, message, args) => {
+module.exports.run = (bot, message, args, discord) => {
   let txt = args.join(' ')
+  let em = new discord.RichEmbed()
+  .setTitle(`Logging Command`)
+  .setDescription(`Sending ${txt} to the baselogger...`)
+  .setImage(bot.user.avatarURL)
+  .setAuthor(bot.user.username)
   if (message.author.id == process.env.oid) {
-        message.channel.send(`Alright, sending ${txt} to the baselogger...`)
+        message.channel.send({ em })
         baselogger(bot, txt, bot.avatarURL) 
   } else {
     message.channel.send("Nope!")
