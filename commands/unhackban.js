5be36afd8430541217428e38abada3e75251fe92
FHGDev <gideon.lingle14@gmail.com>
2018-04-20 17:27:21 -0500

    Update unhackban.js

--- commands/unhackban.js
+++ commands/unhackban.js
@@ -6,6 +6,7 @@ module.exports.run = (bot, message, args, discord) => {
     })
     message.channel.send(`Alright, I unbanned ${user.username}.`)
   }).catch(() => message.channel.send("Sorry, I can't find a user with that ID..."))
+}
   
 
 module.exports.help = {
