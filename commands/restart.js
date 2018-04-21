59e80f54e376eeed17471ef953540f77668f219c
FHGDev <gideon.lingle14@gmail.com>
2018-04-21 18:31:55 -0500

    Update restart.js

--- commands/restart.js
+++ commands/restart.js
@@ -1,9 +1,15 @@
 module.exports.run = (bot, message, args, discord) => {
 let em = new discord.RichEmbed()
-.setTitle("Himiachi Restarting...")
+.setTitle("Hulkbot Restarting...")
 .setDescription(":wave: Restarting...")
 .setThumbnail(bot.user.avatarURL)
 .setImage("https://media1.giphy.com/media/WKJpCXfvBHyla/giphy.gif")
+.setTimestamp()
+.setColor("GOLD")
+let embed = new discord.RichEmbed()
+.setTitle("Hulkbot Restarted!")
+.setDescription(":thumbsup: Hulkbot Successfully Restarted.")
+.setThumbnail(bot.user.avatarURL)
 .setTimestamp()
 .setColor("GOLD")
 
@@ -11,6 +17,8 @@ let em = new discord.RichEmbed()
     channel.send({embed: em})
     .then(() => bot.destroy())
     .then(() => bot.login(process.env.botToken))
+    .catch(err => console.error(err))
+    .then(() => message.channel.send({embed: embed}))
   }
   
   restart(message.channel)
