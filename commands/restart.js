c67b4291fae23af1233e5ea9cac8ca1a09e4a9f5
FHGDev <gideon.lingle14@gmail.com>
2018-04-21 17:57:28 -0500

    Update restart.js

--- commands/restart.js
+++ commands/restart.js
@@ -1,21 +1,18 @@
 module.exports.run = (bot, message, args, discord) => {
+let em = new discord.RichEmbed()
+.setTitle("Hulkbot Restarting...")
+.setDescription(":wave: Restarting...")
+.setThumbnail(bot.user.avatarURL)
+.setTimestamp()
+.setColor("GOLD")
+
   function restart(channel) {
-  let em = new discord.RichEmbed()
-  .setTitle("Hulkbot Restarting...")
-  .setDescription(":wave: Restarting...")
-  .setColor("BLUE")
-  .setTimestamp()
-  .setThumbnail("https://media1.giphy.com/media/WKJpCXfvBHyla/giphy.gif")
-  channel.send({embed: em})
-    bot.destroy().then(() => {
-      bot.login(process.env.token)
-    })
+    channel.send({embed: em})
+    .then(() => bot.destroy())
+    .then(() => bot.login(process.env.token))
   }
-  if (message.author.id == process.env.oid) {
-    restart(message.channel)
-  } else {
-    message.channel.send("Nope!")
-  }
+  
+  restart(message.channel)
 }
 
 module.exports.help = {
