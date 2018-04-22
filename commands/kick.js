d40a724fcbe5435a12df27d1a1828d3f54e8ec25
FHGDev <gideon.lingle14@gmail.com>
2018-04-21 19:15:13 -0500

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,17 +1,28 @@
 const config = require('../config.json')
 const prefix = config.prefix
+const {baselogger} = require('../logger.js')
 
 module.exports.run = (bot, message, args, discord) => {
  let member = message.mentions.members.first();
  let channel = message.channel
- let reason = args[2]
+ let reason = args.slice(1).join(' ')
  let embed = new discord.RichEmbed()
  .setTitle(`${bot.user.username} Kick System`)
- .setDescription(`${member.displayName} was successfully kicked.`)
+ .setDescription(`${member.username} was kicked for the reason ${reason}`)
+ if (member.displayName) {
+   embed.setDescription(`${member.displayName} was kicked for the reason ${reason}`)
+ }
+ 
+ if (!reason) {
+  embed.setDescription(`${member.displayName} was successfully kicked.`)
+ }
  
   if (message.member.hasPermission("KICK_MEMBERS")) {
    
-    member.kick(reason).then(channel.send({ embed }))
+    member.kick(reason).then(channel.send({ embed })).catch(err => {
+      console.error(err)
+      baselogger(bot, `A kick failed in the guild ${member.guild.name}. Attempting to kick: ${member.username}. Error Message: ${err}`, message.guild.iconURL)
+    })
   } else {
     message.channel.send("You don't have permission to kick people. If there's someone who needs to be kicked, contact an administrator or a moderator.")
   } 
@@ -19,6 +30,6 @@ module.exports.run = (bot, message, args, discord) => {
 
 module.exports.help = {
   name: "kick",
-  usage: "h!kick @user",
+  usage: "h!kick {@user} [reason]",
   information: "Kicks a mentioned user."
 }
