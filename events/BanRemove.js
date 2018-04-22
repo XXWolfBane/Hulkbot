6d59ecee2bd92b76f2635ac69e07d81823c47994
FHGDev <gideon.lingle14@gmail.com>
2018-04-22 10:46:46 -0500

    Update BanRemove.js

--- events/BanRemove.js
+++ events/BanRemove.js
@@ -1,26 +1,21 @@
+const discord = require('discord.js')
+
 module.exports = (bot, guild, member) => {
   member.send(`${member.username}, you have now been unbanned from ${guild.name}!`)
+  const lognames = ["guild-maintenance", "log", "bot-log", "hulkbot-log"]
   
-  const log1="log",
-  log2="hulkbot-logs",
-  log3="audit",
-  log4="bot-audit",
-  log5="guild-maintenance"
-  
-  if (guild.channels.exists(log1)) {
-  guild.channels.find('name', log1).then(l => {
-    l.send(`${member.displayName} has been unbanned from ${guild.name}`)
-  })} else {
-  guild.channels.find('name', log2).then(l => {
-    l.send(`${member.displayName} has been unbanned from ${guild.name}`)
-  })} else {
-  guild.channels.find('name', log3).then(l => {
-    l.send(`${member.displayName} has been unbanned from ${guild.name}`)
-  })} else {
-  guild.channels.find('name', log4).then(l => {
-    l.send(`${member.displayName} has been unbanned from ${guild.name}`)
-  })} else {
-  guild.channels.find('name', log5).then(l => {
-    l.send(`${member.displayName} has been unbanned from ${guild.name}`)
-  })}
+  guild.channels.forEach(channel => {
+    if (lognames.includes(channel.name)) {
+      let embed = new discord.RichEmbed()
+      .setTitle("Hulkbot Logger")
+      .setDescription(`${member.username} was just unbanned from ${guild.name}`)
+      .setTimestamp()
+      .setColor("BLUE")
+      .setThumbnail(member.avatarURL)
+      .setFooter(`${member.username} unbanned`)
+      channel.send({embed: embed})
+    } else {
+      return;    
+    }
+  })
 }
