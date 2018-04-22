48270b9c69f7394b01d9e6f80ee533c28b6e431e
FHGDev <gideon.lingle14@gmail.com>
2018-04-21 21:02:52 -0500

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -1,19 +1,21 @@
+const pak = require('../package.json')
+
 module.exports.run = (bot, message, args, discord) => {
   let embed = new discord.RichEmbed()
   .setTitle('Hulkbot Status')
   .setColor(`RANDOM`)
   .setDescription(`Bot Status: Online
-  Bot Servercount: ${bot.guilds.array().length}
-  Bot Usercount: ${bot.users.array().length}
-  Bot Channelcount: ${bot.channels.array().length}
+  Bot Servercount: ${bot.guilds.size}
+  Bot Usercount: ${bot.users.size}
+  Bot Channelcount: ${bot.channels.size}
   Memory Usage: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
-  Discord.js Version 11.3.0
+  Discord.js Version ${pak.dependencies.discord.js}
   Node Version 8.9.4
   Source Code: [GitHub](https://github.com/FHGDev/JSHulkbot)
   Hulkbot Website: [Website](https://bot.hulkbot.ml/home)`)
-  .setFooter(new Date())
-  
-  message.channel.send({ embed })
+  .setFooter(`Hulkbot Status`)
+  .setTimestamp()  
+  message.channel.send({embed: embed})
 }
 
 module.exports.help = {
