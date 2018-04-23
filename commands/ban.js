abdce472b96c7651f3586823c3d3f717a7ab3d6f
FHGDev <gideon.lingle14@gmail.com>
2018-04-23 14:14:45 -0500

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -2,7 +2,6 @@ const fs = require('fs')
 const config = require('../config.json')
 
 module.exports.run = (bot, message, args, discord) => {
-if (message.member.hasPermission("BAN_MEMBERS")) {
     let reason = args.slice(1).join(" ");
     let member = message.mentions.members.first();
     member.ban(reason).catch(console.error)
@@ -14,9 +13,6 @@ if (message.member.hasPermission("BAN_MEMBERS")) {
     config.banreason = reason;
     fs.writeFile('../config.json', JSON.stringify(reason), (err) => console.error)
     message.channel.send({embed})
-  } else {
-    message.channel.send(`ADMIN ONLY, CHUMP!`)
-  }
 }
 
 module.exports.help = {
